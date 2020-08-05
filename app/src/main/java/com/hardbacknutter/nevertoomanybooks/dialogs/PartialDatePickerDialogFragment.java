ac9a3b02d9f5296f8d230f0e3acb6925ae6bd4de
tfonteyn <user@localhost>
2020-08-05 15:27:19 +0100

    PartialDatePickerDialogFragment converted to floating/fullscreen like other dialogs

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/dialogs/PartialDatePickerDialogFragment.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/dialogs/PartialDatePickerDialogFragment.java
@@ -25,10 +25,8 @@
  * You should have received a copy of the GNU General Public License
  * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.hardbacknutter.nevertoomanybooks.dialogs.date;
+package com.hardbacknutter.nevertoomanybooks.dialogs;
 
-import android.annotation.SuppressLint;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.util.Log;
@@ -36,14 +34,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
 
+import androidx.annotation.CallSuper;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
-import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.time.LocalDate;
@@ -54,7 +54,6 @@ import java.util.Locale;
 
 import com.hardbacknutter.nevertoomanybooks.BuildConfig;
 import com.hardbacknutter.nevertoomanybooks.R;
-import com.hardbacknutter.nevertoomanybooks.dialogs.StandardDialogs;
 import com.hardbacknutter.nevertoomanybooks.utils.LocaleUtils;
 
 /**
@@ -67,18 +66,39 @@ import com.hardbacknutter.nevertoomanybooks.utils.LocaleUtils;
  * A final validity check is done when trying to accept the date.
  */
 public class PartialDatePickerDialogFragment
-        extends BaseDatePickerDialogFragment {
+        extends BaseDialogFragment {
 
     /** Log tag. */
     public static final String TAG = "PartialDatePickerDialog";
     public static final String REQUEST_KEY = TAG + ":rk";
+
+    /** a standard sql style date string, must be correct. */
+    static final String BKEY_DATE = TAG + ":date";
 
     /** Displayed to user: unset month. */
     private static final String UNKNOWN_MONTH = "---";
     /** Displayed to user: unset day. */
     private static final String UNKNOWN_DAY = "--";
 
+    /** or the date split into components, which can be partial. */
+    private static final String SIS_YEAR = TAG + ":year";
+    /** range: 0: 'not set' or 1..12. */
+    private static final String SIS_MONTH = TAG + ":month";
+    /** range: 0: 'not set' or 1..31. */
+    private static final String SIS_BKEY_DAY = TAG + ":day";
+
+    /** Currently displayed; {@code 0} if empty/invalid. */
+    int mYear;
+    /**
+     * Currently displayed; {@code 0} if invalid/empty.
+     * <strong>IMPORTANT:</strong> 1..12 based. (the jdk internals expect 0..11).
+     */
+    int mMonth;
+    /** Currently displayed; {@code 0} if empty/invalid. */
+    int mDay;
+
     private NumberPicker mDayPicker;
+
     /** This listener is called after <strong>any change</strong> made to the pickers. */
     private final NumberPicker.OnValueChangeListener mOnValueChangeListener =
             (picker, oldVal, newVal) -> {
@@ -110,6 +130,13 @@ public class PartialDatePickerDialogFragment
 
     @StringRes
     private int mDialogTitleId;
+
+    /**
+     * No-arg constructor for OS use.
+     */
+    public PartialDatePickerDialogFragment() {
+        super(R.layout.dialog_partial_date_picker);
+    }
 
     /**
      * Constructor.
@@ -150,21 +177,37 @@ public class PartialDatePickerDialogFragment
         }
 
         setupDate(savedInstanceState);
-        // can't have a null year. (but month/day can be null)
+        // can't have a 0 year. (but month/day can be 0)
         // The user can/should use the "clear" button if they want no date at all.
         if (mYear == 0) {
             mYear = LocalDate.now().getYear();
         }
     }
 
-    @NonNull
     @Override
-    public Dialog onCreateDialog(@Nullable final Bundle savedInstanceState) {
-        @SuppressLint("InflateParams")
-        final View root = getLayoutInflater().inflate(R.layout.dialog_partial_date_picker, null);
+    public void onViewCreated(@NonNull final View view,
+                              @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        final View root = getView();
         // Ensure components match current Locale order
+        //noinspection ConstantConditions
         reorderPickers(root);
+
+
+        final Toolbar toolbar = root.findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(v -> dismiss());
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.MENU_SAVE) {
+                if (saveChanges()) {
+                    dismiss();
+                }
+                return true;
+            }
+            return false;
+        });
+        toolbar.setTitle(mDialogTitleId != 0 ? mDialogTitleId : R.string.action_edit);
+
 
         final NumberPicker yearPicker = root.findViewById(R.id.year);
         // 0: 'not set'
@@ -195,45 +238,27 @@ public class PartialDatePickerDialogFragment
         monthPicker.setValue(mMonth);
         mDayPicker.setValue(mDay);
         updateDaysInMonth();
-
-        //noinspection ConstantConditions
-        return new MaterialAlertDialogBuilder(getContext())
-                .setView(root)
-                .setTitle(mDialogTitleId != 0 ? mDialogTitleId : R.string.action_edit)
-                // no listeners. They must be set in the onResume of the fragment.
-                .setNegativeButton(android.R.string.cancel, null)
-                .setNeutralButton(R.string.action_clear, null)
-                .setPositiveButton(android.R.string.ok, null)
-                .create();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        final AlertDialog dialog = (AlertDialog) getDialog();
-        if (dialog != null) {
-            // Set the dialog OnClickListener in onResume.
-            // This allows us to validate the fields without
-            // having the dialog close on us after the user clicks a button.
-            dialog.getButton(Dialog.BUTTON_NEGATIVE).setOnClickListener(
-                    v -> dismiss());
-            dialog.getButton(Dialog.BUTTON_NEUTRAL).setOnClickListener(
-                    v -> DatePickerResultsListener.sendResult(this, REQUEST_KEY, 0, 0, 0));
-            dialog.getButton(Dialog.BUTTON_POSITIVE).setOnClickListener(v -> {
-                if (mDay != 0 && mMonth == 0) {
-                    Snackbar.make(v, R.string.warning_if_day_set_month_and_year_must_be,
-                                  Snackbar.LENGTH_LONG).show();
+    private boolean saveChanges() {
+        if (mDay != 0 && mMonth == 0) {
+            //noinspection ConstantConditions
+            Snackbar.make(getView(), R.string.warning_if_day_set_month_and_year_must_be,
+                          Snackbar.LENGTH_LONG).show();
 
-                } else if (mMonth != 0 && mYear == 0) {
-                    Snackbar.make(v, R.string.warning_if_month_set_year_must_be,
-                                  Snackbar.LENGTH_LONG).show();
+        } else if (mMonth != 0 && mYear == 0) {
+            //noinspection ConstantConditions
+            Snackbar.make(getView(), R.string.warning_if_month_set_year_must_be,
+                          Snackbar.LENGTH_LONG).show();
 
-                } else {
-                    DatePickerResultsListener.sendResult(this, REQUEST_KEY, mYear, mMonth, mDay);
-                }
-            });
+        } else {
+            OnResultListener.sendResult(this, REQUEST_KEY, mYear, mMonth, mDay);
+            return true;
         }
+
+        return false;
     }
+
 
     /**
      * Generate the month names (abbreviated). There are 13: first entry being 'unknown'.
@@ -321,5 +346,120 @@ public class PartialDatePickerDialogFragment
                     break;
             }
         }
+    }
+
+    /**
+     * Common setup for the pickers.
+     *
+     * @param savedInstanceState from #onCreateDialog
+     */
+    void setupDate(@Nullable final Bundle savedInstanceState) {
+
+        final Bundle args = savedInstanceState != null ? savedInstanceState : requireArguments();
+        if (args.containsKey(BKEY_DATE)) {
+            // BKEY_DATE is only present in the original args
+            parseDate(args.getString(BKEY_DATE));
+        } else {
+            // These are only present in the savedInstanceState
+            mYear = args.getInt(SIS_YEAR);
+            mMonth = args.getInt(SIS_MONTH);
+            mDay = args.getInt(SIS_BKEY_DAY);
+        }
+    }
+
+    @Override
+    @CallSuper
+    public void onSaveInstanceState(@NonNull final Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SIS_YEAR, mYear);
+        outState.putInt(SIS_MONTH, mMonth);
+        outState.putInt(SIS_BKEY_DAY, mDay);
+    }
+
+    /**
+     * Parse the input ISO date string into the individual components.
+     * <p>
+     * Note we don't use {@link com.hardbacknutter.nevertoomanybooks.utils.DateParser}
+     * as we the current implementation always returns full dates.
+     * Here, we explicitly need to support partial dates.
+     *
+     * <ul>Allowed formats:
+     *      <li>yyyy-mm-dd time</li>
+     *      <li>yyyy-mm-dd</li>
+     *      <li>yyyy-mm</li>
+     *      <li>yyyy</li>
+     * </ul>
+     *
+     * @param dateString SQL formatted (partial) date, can be {@code null}.
+     */
+    private void parseDate(@Nullable final String dateString) {
+        if (dateString == null || dateString.isEmpty()) {
+            mYear = 0;
+            mMonth = 0;
+            mDay = 0;
+            return;
+        }
+
+        int yyyy = 0;
+        int mm = 0;
+        int dd = 0;
+        try {
+            final String[] dateAndTime = dateString.split(" ");
+            final String[] date = dateAndTime[0].split("-");
+            yyyy = Integer.parseInt(date[0]);
+            if (date.length > 1) {
+                mm = Integer.parseInt(date[1]);
+            }
+            if (date.length > 2) {
+                dd = Integer.parseInt(date[2]);
+            }
+        } catch (@NonNull final NumberFormatException ignore) {
+            // ignore. Any values we did get, are used.
+        }
+
+        mYear = yyyy;
+        mMonth = mm;
+        mDay = dd;
+    }
+
+    /**
+     * Listener interface to receive notifications a date was picked.
+     */
+    public interface OnResultListener
+            extends FragmentResultListener {
+
+        /* private. */ String YEAR = "year";
+        /* private. */ String MONTH = "month";
+        /* private. */ String DAY = "day";
+
+        static void sendResult(@NonNull final Fragment fragment,
+                               @NonNull final String requestKey,
+                               final int year,
+                               final int month,
+                               final int day) {
+            final Bundle result = new Bundle();
+            result.putInt(YEAR, year);
+            result.putInt(MONTH, month);
+            result.putInt(DAY, day);
+
+            fragment.getParentFragmentManager().setFragmentResult(requestKey, result);
+        }
+
+        @Override
+        default void onFragmentResult(@NonNull final String requestKey,
+                                      @NonNull final Bundle result) {
+            onResult(result.getInt(YEAR), result.getInt(MONTH), result.getInt(DAY));
+        }
+
+        /**
+         * Callback handler with the user's selection.
+         *
+         * @param year  4 digit year, or {@code 0} for none
+         * @param month 1..12 based, or {@code 0} for none
+         * @param day   1..31 based, or {@code 0} for none
+         */
+        void onResult(int year,
+                      int month,
+                      int day);
     }
 }
