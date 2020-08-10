20d7c4b7c0eb999a8bc823c551c586bd6ffa778e
tfonteyn <user@localhost>
2020-08-10 13:29:26 +0100

    styles

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/AuthorWorksFragment.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/AuthorWorksFragment.java
@@ -4,14 +4,6 @@
  *
  * This file is part of NeverTooManyBooks.
  *
- * In August 2018, this project was forked from:
- * Book Catalogue 5.2.2 @2016 Philip Warner & Evan Leybourn
- *
- * Without their original creation, this project would not exist in its
- * current form. It was however largely rewritten/refactored and any
- * comments on this fork should be directed at HardBackNutter and not
- * at the original creators.
- *
  * NeverTooManyBooks is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -394,12 +386,12 @@ public class AuthorWorksFragment
         TocAdapter(@NonNull final Context context) {
             super();
             mInflater = LayoutInflater.from(context);
+
+            final Resources.Theme theme = context.getTheme();
             mDrawableOn = context.getResources().getColorStateList(
-                    AttrUtils.getResId(context, R.attr.appIconTint),
-                    context.getTheme());
+                    AttrUtils.getResId(context, R.attr.colorControlNormal), theme);
             mDrawableOff = context.getResources().getColorStateList(
-                    android.R.color.transparent,
-                    context.getTheme());
+                    android.R.color.transparent, theme);
         }
 
         @NonNull
--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/settings/GlobalPreferenceFragment.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/settings/GlobalPreferenceFragment.java
@@ -4,14 +4,6 @@
  *
  * This file is part of NeverTooManyBooks.
  *
- * In August 2018, this project was forked from:
- * Book Catalogue 5.2.2 @2016 Philip Warner & Evan Leybourn
- *
- * Without their original creation, this project would not exist in its
- * current form. It was however largely rewritten/refactored and any
- * comments on this fork should be directed at HardBackNutter and not
- * at the original creators.
- *
  * NeverTooManyBooks is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -137,7 +129,7 @@ public class GlobalPreferenceFragment
                 d.setTint(getContext().getColor(R.color.red_500));
             } else {
                 //noinspection ConstantConditions
-                d.setTint(AttrUtils.getColorInt(getContext(), R.attr.appIconTint));
+                d.setTint(AttrUtils.getColorInt(getContext(), R.attr.colorControlNormal));
             }
             preference.setIcon(d);
         }
