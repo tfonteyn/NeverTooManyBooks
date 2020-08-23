2ca4ded698ab9d7f30038a48bdea0bb1e0d0ca12
tfonteyn <user@localhost>
2020-08-23 20:56:28 +0100

    extra comment

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/backup/csv/CsvArchiveReader.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/backup/csv/CsvArchiveReader.java
@@ -4,14 +4,6 @@
  *
  * This file is part of NeverTooManyBooks.
  *
- * In August 2018, this project was forked from:
- * Book Catalogue 5.2.2 @2016 Philip Warner & Evan Leybourn
- *
- * Without their original creation, this project would not exist in its
- * current form. It was however largely rewritten/refactored and any
- * comments on this fork should be directed at HardBackNutter and not
- * at the original creators.
- *
  * NeverTooManyBooks is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -47,6 +39,10 @@ import com.hardbacknutter.nevertoomanybooks.backup.base.Options;
 import com.hardbacknutter.nevertoomanybooks.backup.base.ReaderEntity;
 import com.hardbacknutter.nevertoomanybooks.tasks.ProgressListener;
 
+/**
+ * A minimal implementation of {@link ArchiveReader} which reads a plain CSV file with books.
+ * {@link Options} are NOT supported for now (hardcoded to {@link Options#BOOKS} only).
+ */
 public class CsvArchiveReader
         implements ArchiveReader {
 
