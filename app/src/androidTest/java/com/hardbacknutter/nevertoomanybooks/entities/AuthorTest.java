3bdb210cc1534d8fe1e543ff8692f91e20d6f29e
tfonteyn <user@localhost>
2020-09-02 18:41:02 +0100

    first androidTest

---
+++ app/src/androidTest/java/com/hardbacknutter/nevertoomanybooks/entities/AuthorTest.java
@@ -0,0 +1,52 @@
+/*
+ * @Copyright 2020 HardBackNutter
+ * @License GNU General Public License
+ *
+ * This file is part of NeverTooManyBooks.
+ *
+ * NeverTooManyBooks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeverTooManyBooks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.hardbacknutter.nevertoomanybooks.entities;
+
+
+import android.os.Parcel;
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SmallTest
+public class AuthorTest {
+
+    /**
+     * Not really an important test; just an exercise in creating an "androidTest".
+     */
+    @Test
+    public void parcelling() {
+        final Author author = Author.from("Isaac Asimov");
+
+        final Parcel parcel = Parcel.obtain();
+        author.writeToParcel(parcel, author.describeContents());
+        parcel.setDataPosition(0);
+        Author pAuthor = Author.CREATOR.createFromParcel(parcel);
+
+        assertEquals(pAuthor.getId(), author.getId());
+        assertEquals(pAuthor.getFamilyName(), author.getFamilyName());
+        assertEquals(pAuthor.getGivenNames(), author.getGivenNames());
+        assertEquals(pAuthor.isComplete(), author.isComplete());
+        assertEquals(pAuthor.getType(), author.getType());
+    }
+}
