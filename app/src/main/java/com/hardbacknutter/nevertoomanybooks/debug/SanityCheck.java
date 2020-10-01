a1651f66a23a154c3e977a2dc839d92692b1b694
tfonteyn <user@localhost>
2020-10-01 21:02:10 +0100

    an alternative/extension of Objects.require...

---
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/debug/SanityCheck.java
@@ -0,0 +1,70 @@
+/*
+ * @Copyright 2020 HardBackNutter
+ * @License GNU General Public License
+ *
+ * This file is part of NeverTooManyBooks.
+ *
+ * NeverTooManyBooks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeverTooManyBooks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.hardbacknutter.nevertoomanybooks.debug;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Collection;
+
+public class SanityCheck {
+
+    @SuppressWarnings("UnusedReturnValue")
+    public static long requireValue(final long value,
+                                    @Nullable final String message) {
+        if (value < 1) {
+            throw new MissingValueException(message);
+        }
+        return value;
+    }
+
+    @SuppressWarnings("UnusedReturnValue")
+    @NonNull
+    public static String requireValue(@Nullable final String s,
+                                      @Nullable final String message) {
+        if (s == null || s.isEmpty()) {
+            throw new MissingValueException(message);
+        }
+        return s;
+    }
+
+    @SuppressWarnings("UnusedReturnValue")
+    @NonNull
+    public static <T extends Collection<?>> T requireValue(@Nullable final T s,
+                                                           @Nullable final String message) {
+        if (s == null || s.isEmpty()) {
+            throw new MissingValueException(message);
+        }
+        return s;
+    }
+
+    public static class MissingValueException
+            extends NullPointerException {
+
+        private static final long serialVersionUID = 902891333141380801L;
+
+        MissingValueException() {
+        }
+
+        public MissingValueException(@Nullable final String s) {
+            super(s);
+        }
+    }
+}
