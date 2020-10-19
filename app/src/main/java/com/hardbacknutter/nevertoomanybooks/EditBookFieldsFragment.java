578fed783330d42c822ec6c82b3165f24b0d87a2
tfonteyn <user@localhost>
2020-10-19 14:48:45 +0100

    made dbg log 'always'

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/EditBookFieldsFragment.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/EditBookFieldsFragment.java
@@ -369,7 +369,7 @@ public class EditBookFieldsFragment
                 Objects.requireNonNull(mScannerModel, ScannerViewModel.TAG);
                 mScannerModel.setScannerStarted(false);
                 if (resultCode == Activity.RESULT_OK) {
-                    if (BuildConfig.DEBUG) {
+                    if (BuildConfig.DEBUG /* always */) {
                         //noinspection ConstantConditions
                         mScannerModel.fakeScanInEmulator(getContext(), data);
                     }
