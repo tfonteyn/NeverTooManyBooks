aaa69cb2b71709a63ba0715d15743d99827c92ca
tfonteyn <user@localhost>
2021-03-07 13:25:30 +0000

    bugfix - wrong import

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/SearchBookUpdatesFragment.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/SearchBookUpdatesFragment.java
@@ -30,12 +30,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
-import android.widget.Toolbar;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.CallSuper;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
