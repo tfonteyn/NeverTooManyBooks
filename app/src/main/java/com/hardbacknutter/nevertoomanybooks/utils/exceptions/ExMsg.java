f52b85d52611f0c7947a7a8bb87b3453c6feefaf
tfonteyn <user@localhost>
2023-03-12 18:31:20 +0000

    moved UpgradeFailedException to core

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/utils/exceptions/ExMsg.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/utils/exceptions/ExMsg.java
@@ -35,6 +35,7 @@ import com.hardbacknutter.nevertoomanybooks.R;
 import com.hardbacknutter.nevertoomanybooks.core.Logger;
 import com.hardbacknutter.nevertoomanybooks.core.LoggerFactory;
 import com.hardbacknutter.nevertoomanybooks.core.database.DaoWriteException;
+import com.hardbacknutter.nevertoomanybooks.core.database.UpgradeFailedException;
 import com.hardbacknutter.nevertoomanybooks.core.network.CredentialsException;
 import com.hardbacknutter.nevertoomanybooks.core.network.HttpNotFoundException;
 import com.hardbacknutter.nevertoomanybooks.core.network.HttpStatusException;
