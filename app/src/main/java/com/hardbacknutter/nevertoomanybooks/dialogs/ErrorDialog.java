fec3dc6ab734728a9afea07c7e141738a9e3de03
tfonteyn <user@localhost>
2023-03-20 12:01:39 +0000

    add full decision logic on how to actually display the error

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/dialogs/ErrorDialog.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/dialogs/ErrorDialog.java
@@ -28,11 +28,13 @@ import androidx.annotation.Nullable;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
+import java.util.Optional;
+
 import com.hardbacknutter.nevertoomanybooks.R;
 import com.hardbacknutter.nevertoomanybooks.utils.exceptions.ExMsg;
 
 /**
- * {@link #show(Context, Throwable, String, String, DialogInterface.OnClickListener)}
+ * {@link #show(Context, Throwable, CharSequence, CharSequence, DialogInterface.OnClickListener)}
  * <p>
  * All other methods are convenience methods which call the above.
  */
@@ -60,7 +62,7 @@ public final class ErrorDialog {
      */
     public static void show(@NonNull final Context context,
                             @NonNull final Throwable e,
-                            @NonNull final String title) {
+                            @NonNull final CharSequence title) {
         show(context, e, title, null, (d, w) -> d.dismiss());
     }
 
@@ -74,7 +76,7 @@ public final class ErrorDialog {
      */
     public static void show(@NonNull final Context context,
                             @NonNull final Throwable e,
-                            @NonNull final String title,
+                            @NonNull final CharSequence title,
                             @NonNull final DialogInterface.OnClickListener closingAction) {
         show(context, e, title, null, closingAction);
     }
@@ -89,8 +91,8 @@ public final class ErrorDialog {
      */
     public static void show(@NonNull final Context context,
                             @NonNull final Throwable e,
-                            @NonNull final String title,
-                            @NonNull final String message) {
+                            @NonNull final CharSequence title,
+                            @NonNull final CharSequence message) {
         show(context, e, title, message, (d, w) -> d.dismiss());
     }
 
@@ -98,55 +100,89 @@ public final class ErrorDialog {
      * Show an error message after an Exception was thrown.
      *
      * @param context       Current context
-     * @param e             The error.
-     *                      Will be {@code null} when called recursively;
-     *                      otherwise must NOT be {@code null}
-     * @param title         optional; Dialog title string resource id; use {@code null} for none
+     * @param e             The error. SHOULD NOT be {@code null}.
+     *                      But MUST be {@code null} when called recursively.
+     * @param title         optional; Dialog title; use {@code null} for none
      * @param message       optional; The message to show; use {@code null} for none
      * @param closingAction to use for the positive button
      */
     public static void show(@NonNull final Context context,
                             @Nullable final Throwable e,
-                            @Nullable final String title,
-                            @Nullable final String message,
+                            @Nullable final CharSequence title,
+                            @Nullable final CharSequence message,
                             @NonNull final DialogInterface.OnClickListener closingAction) {
-
-        String tmpMsg;
-        // Explicitly test on null, as we can call this method recursive (see neutral button)
-        // If we don't we'd end up with a duplicate concatenated message.
-        if (e != null) {
-            tmpMsg = ExMsg.map(context, e)
-                          .map(errorMsg -> message == null || message.isEmpty()
-                                           ? errorMsg
-                                           : errorMsg + "\n\n" + message)
-                          .orElse(message);
-        } else {
-            tmpMsg = message;
-        }
-
-        if (tmpMsg == null || tmpMsg.isEmpty()) {
-            // There is no message whatsoever, freak-out!
-            tmpMsg = context.getString(R.string.error_unknown_long,
-                                       context.getString(R.string.pt_maintenance));
-        }
-
-        final String detailedMessage = tmpMsg;
 
         final MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context)
                 .setIcon(R.drawable.ic_baseline_error_24)
                 .setPositiveButton(android.R.string.ok, closingAction);
 
+        // both are set
+        if (title != null && message != null) {
+            builder.setTitle(title)
+                   .setMessage(message);
+
+            // the exception MAY be absent
+            if (e != null) {
+                // If an exception is available, the "more" button will
+                // show the message concatenated with the actual exception message.
+                // Pass in a null exception so we don't loop.
+                builder.setNeutralButton(R.string.action_more_ellipsis, (d, w) ->
+                        show(context, null,
+                             title, message + "\n\n" + e.getLocalizedMessage(),
+                             closingAction)
+                );
+            }
+            builder.create()
+                   .show();
+            return;
+        }
+
+        // We have a title, but no message
         if (title != null) {
             builder.setTitle(title);
-        }
-        builder.setMessage(detailedMessage);
 
+            // Try to map the exception to a localized/simple message.
+            final Optional<String> mappedMsg = ExMsg.map(context, e);
+            // If we have no mapped message or the exception was null - freak-out!
+            final String message2 = mappedMsg.orElseGet(() -> context.getString(
+                    R.string.error_unknown_long, context.getString(R.string.pt_maintenance)));
+            builder.setMessage(message2);
+
+            // The exception SHOULD be present but we're paranoid.
+            if (e != null) {
+                // If an exception is available, the "more" button will
+                // show the derived message concatenated with the actual exception message.
+                // Pass in a null exception so we don't loop.
+                builder.setNeutralButton(R.string.action_more_ellipsis, (d, w) ->
+                        show(context, null,
+                             title, message2 + "\n\n" + e.getLocalizedMessage(),
+                             closingAction)
+                );
+            }
+            builder.create()
+                   .show();
+            return;
+        }
+
+
+        // worst case, we have no title and no message.
+
+        // Try to map the exception to a localized/simple message.
+        final Optional<String> mappedMsg = ExMsg.map(context, e);
+        // If we have no mapped message or the exception was null - freak-out!
+        final String title2 = mappedMsg.orElseGet(() -> context.getString(
+                R.string.error_unknown_long, context.getString(R.string.pt_maintenance)));
+        // Set it as the title and leave the message itself blank.
+        builder.setTitle(title2);
+
+        // The exception SHOULD be present but we're paranoid.
         if (e != null) {
+            // If an exception is available, the "more" button will
+            // show the actual exception message.
+            // Pass in a null exception so we don't loop.
             builder.setNeutralButton(R.string.action_more_ellipsis, (d, w) ->
-                    show(context,
-                         null, title,
-                         detailedMessage + "\n\n" + e.getLocalizedMessage(),
-                         // Pass in null so we don't loop
+                    show(context, null,
+                         title2, e.getLocalizedMessage(),
                          closingAction)
             );
         }
