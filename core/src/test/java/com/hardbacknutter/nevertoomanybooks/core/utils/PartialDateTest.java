71553bcf1a74e2b2a0a07eac1a272508cc91f5d0
tfonteyn <user@localhost>
2023-03-23 10:08:26 +0000

    moved to core

--- core/src/test/java/com/hardbacknutter/nevertoomanybooks/core/utils/PartialDateTest.java
+++ core/src/test/java/com/hardbacknutter/nevertoomanybooks/core/utils/PartialDateTest.java
@@ -17,9 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.hardbacknutter.nevertoomanybooks.utils.dates;
-
-import com.hardbacknutter.nevertoomanybooks.core.utils.PartialDate;
+package com.hardbacknutter.nevertoomanybooks.core.utils;
 
 import org.junit.jupiter.api.Test;
 
