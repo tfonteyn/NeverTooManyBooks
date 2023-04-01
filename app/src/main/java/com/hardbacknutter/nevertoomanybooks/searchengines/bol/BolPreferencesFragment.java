da7b75c8fe84fd47a9ab51ddee6ae48997b5c968
tfonteyn <user@localhost>
2023-04-01 19:02:34 +0100

    added 'country' selection

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/bol/BolPreferencesFragment.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/bol/BolPreferencesFragment.java
@@ -23,6 +23,9 @@ package com.hardbacknutter.nevertoomanybooks.searchengines.bol;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
+
+import java.util.Locale;
 
 import com.hardbacknutter.nevertoomanybooks.R;
 import com.hardbacknutter.nevertoomanybooks.settings.BasePreferenceFragment;
@@ -30,10 +33,34 @@ import com.hardbacknutter.nevertoomanybooks.settings.BasePreferenceFragment;
 public class BolPreferencesFragment
         extends BasePreferenceFragment {
 
+    private static final String DEF_COUNTRY = "";
+
+    private final CharSequence[] entries = new CharSequence[3];
+    private final CharSequence[] entryValues = {DEF_COUNTRY, "be", "nl"};
+
     @Override
     public void onCreatePreferences(@Nullable final Bundle savedInstanceState,
                                     @Nullable final String rootKey) {
         super.onCreatePreferences(savedInstanceState, rootKey);
         setPreferencesFromResource(R.xml.preferences_site_bol, rootKey);
+
+        entries[0] = getString(R.string.lbl_system_default);
+        entries[1] = new Locale("nl", "BE").getDisplayCountry();
+        entries[2] = new Locale("nl", "NL").getDisplayCountry();
+
+        final ListPreference p = findPreference(BolSearchEngine.PK_BOL_COUNTRY);
+        //noinspection ConstantConditions
+        p.setEntries(entries);
+        p.setEntryValues(entryValues);
+        p.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
+
+        // The ListPreference has an issue that the initial value is set during the inflation
+        // step. At that time, the default value is ONLY available from xml.
+        // Internally it will then use this to set the value.
+        // Workaround: set the default, and if the pref has no value, set it as well...
+        p.setDefaultValue(DEF_COUNTRY);
+        if (p.getValue() == null) {
+            p.setValue(DEF_COUNTRY);
+        }
     }
 }
