3fe4e895969a042ecfd4706406346357bbd25428
tfonteyn <user@localhost>
2023-04-01 15:39:03 +0100

    add BOL preference screen

---
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/bol/BolPreferencesFragment.java
@@ -0,0 +1,39 @@
+/*
+ * @Copyright 2018-2023 HardBackNutter
+ * @License GNU General Public License
+ *
+ * This file is part of NeverTooManyBooks.
+ *
+ * NeverTooManyBooks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeverTooManyBooks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hardbacknutter.nevertoomanybooks.searchengines.bol;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.hardbacknutter.nevertoomanybooks.R;
+import com.hardbacknutter.nevertoomanybooks.settings.BasePreferenceFragment;
+
+public class BolPreferencesFragment
+        extends BasePreferenceFragment {
+
+    @Override
+    public void onCreatePreferences(@Nullable final Bundle savedInstanceState,
+                                    @Nullable final String rootKey) {
+        super.onCreatePreferences(savedInstanceState, rootKey);
+        setPreferencesFromResource(R.xml.preferences_site_bol, rootKey);
+    }
+}
