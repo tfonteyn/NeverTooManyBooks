b35d4594136d23dbe4b4d8e0936e423a500ab3d0
tomf <user@localhost>
2023-09-24 20:45:51 +0100

    refactor/add a 'reverse' method

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/utils/ReorderHelper.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/utils/ReorderHelper.java
@@ -58,6 +58,7 @@ public final class ReorderHelper {
      */
 
     public static final String PK_SORT_TITLE_REORDERED = "sort.title.reordered";
+    private static final String SUFFIX_SEPARATOR = ", ";
 
     /**
      * Cache for the pv_reformat_titles_prefixes strings.
@@ -164,7 +165,7 @@ public final class ReorderHelper {
      * @param context     Current context
      * @param title       to reorder
      * @param firstLocale to try first
-     * @param localeList  to try after
+     * @param localeList  to try if the the firstLocale fails
      *
      * @return reordered title, or the original if the pattern was not found
      */
@@ -180,26 +181,11 @@ public final class ReorderHelper {
             return title;
         }
 
-        // Create a NEW list, and add optional prefix at the start, and Locale.ENGLISH at the end
-        final List<Locale> locales = new ArrayList<>(localeList);
-        if (firstLocale != null) {
-            locales.add(0, firstLocale);
-        }
-        locales.add(Locale.ENGLISH);
-
+        final List<Locale> locales = concatLocales(firstLocale, localeList);
         for (final Locale locale : locales) {
-            // Creating the pattern is slow, so we cache it for every Locale.
-            String words = localePrefixMap.get(locale);
-            if (words == null) {
-                words = appLocaleSupplier.get()
-                                         .getLocalizedResources(context, locale)
-                                         .getString(R.string.pv_reformat_titles_prefixes);
-                localePrefixMap.put(locale, words);
-            }
-
             // case sensitive, see notes in
             // src/main/res/values/string.xml/pv_reformat_titles_prefixes
-            if (words.contains(titleWords[0])) {
+            if (getWords(context, locale).contains(titleWords[0])) {
                 final StringBuilder newTitle = new StringBuilder();
                 for (int i = 1; i < titleWords.length; i++) {
                     if (i != 1) {
@@ -207,10 +193,89 @@ public final class ReorderHelper {
                     }
                     newTitle.append(titleWords[i]);
                 }
-                newTitle.append(", ").append(titleWords[0]);
+                newTitle.append(SUFFIX_SEPARATOR).append(titleWords[0]);
                 return newTitle.toString();
             }
         }
         return title;
     }
+
+
+    /**
+     * Check if the given text is reordered.
+     * If not, just return the text as-is; otherwise reverse the reordering.
+     *
+     * @param context     Current context
+     * @param text        to process
+     * @param firstLocale to try first
+     * @param localeList  to try after
+     *
+     * @return the un-reordered text
+     */
+    @NonNull
+    public String reverse(@NonNull final Context context,
+                          @NonNull final String text,
+                          @Nullable final Locale firstLocale,
+                          @NonNull final List<Locale> localeList) {
+        final List<Locale> locales = concatLocales(firstLocale, localeList);
+        for (final Locale locale : locales) {
+            final String[] words = getWords(context, locale).split("\\|");
+            for (final String word : words) {
+                if (text.endsWith(SUFFIX_SEPARATOR + word)) {
+                    // This is the (hopefully) original/actual title.
+                    final String reconstructed =
+                            word + " " + text.substring(0, word.length() + 2);
+                    // Now reorder it AGAIN, and check if it matches the original text.
+                    final String reordered =
+                            reorder(context, reconstructed, firstLocale, localeList);
+                    if (text.equalsIgnoreCase(reordered)) {
+                        // We have a good chance that this is the original title.
+                        // The first character of the 'word' and the original 'text'
+                        // might however be wrong. Leave that to the user...
+                        return reconstructed;
+                    }
+                }
+            }
+        }
+
+        // No changes
+        return text;
+    }
+
+
+    @NonNull
+    private String getWords(@NonNull final Context context,
+                            @NonNull final Locale locale) {
+        // getLocalizedResources is slow, so we cache it for every Locale.
+        String words = localePrefixMap.get(locale);
+        if (words == null) {
+            words = appLocaleSupplier.get()
+                                     .getLocalizedResources(context, locale)
+                                     .getString(R.string.pv_reformat_titles_prefixes);
+            localePrefixMap.put(locale, words);
+        }
+        return words;
+    }
+
+    /**
+     * Prefix the given 'localeList' with the (optional) 'firstLocale',
+     * and suffix it with {@link Locale#ENGLISH}.
+     *
+     * @param firstLocale (optional) prefix
+     * @param localeList  main list
+     *
+     * @return concatenated/final Locale list
+     */
+    @NonNull
+    private List<Locale> concatLocales(@Nullable final Locale firstLocale,
+                                       @NonNull final List<Locale> localeList) {
+        // Create a NEW list, and add optional prefix at the start, and Locale.ENGLISH at the end
+        final List<Locale> locales = new ArrayList<>(localeList);
+        if (firstLocale != null) {
+            locales.add(0, firstLocale);
+        }
+        locales.add(Locale.ENGLISH);
+        return locales;
+    }
+
 }
