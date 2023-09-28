4a092dd2aa5def834a6cc40a693a81487cac5571
tomf <user@localhost>
2023-09-28 12:27:29 +0100

    moved all BOL tests to androidTest

--- app/src/androidTest/java/com/hardbacknutter/nevertoomanybooks/searchengines/bol/ParseTest.java
+++ app/src/androidTest/java/com/hardbacknutter/nevertoomanybooks/searchengines/bol/ParseTest.java
@@ -24,9 +24,9 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
-import com.hardbacknutter.nevertoomanybooks.JSoupBase;
+import com.hardbacknutter.nevertoomanybooks.BaseDBTest;
 import com.hardbacknutter.nevertoomanybooks.TestProgressListener;
-import com.hardbacknutter.nevertoomanybooks._mocks.os.BundleMock;
+import com.hardbacknutter.nevertoomanybooks.core.database.DaoWriteException;
 import com.hardbacknutter.nevertoomanybooks.core.network.CredentialsException;
 import com.hardbacknutter.nevertoomanybooks.core.parsers.RealNumberParser;
 import com.hardbacknutter.nevertoomanybooks.core.storage.StorageException;
@@ -39,45 +39,48 @@ import com.hardbacknutter.nevertoomanybooks.searchengines.EngineId;
 import com.hardbacknutter.nevertoomanybooks.searchengines.SearchException;
 
 import org.jsoup.nodes.Document;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-public class BolTest
-        extends JSoupBase {
 
-    private static final String TAG = "BolTest";
+/** @noinspection MissingJavadoc */
+public class ParseTest
+        extends BaseDBTest {
+
+    private static final String TAG = "ParseTest";
     private static final String UTF_8 = "UTF-8";
     private BolSearchEngine searchEngine;
-    private Book book;
 
-    @BeforeEach
+    @Before
     public void setup()
-            throws Exception {
+            throws DaoWriteException, StorageException {
         super.setup();
-        book = new Book(BundleMock.create());
+
         searchEngine = (BolSearchEngine) EngineId.Bol.createSearchEngine(context);
         searchEngine.setCaller(new TestProgressListener(TAG));
     }
 
     /** Network access! */
     @Test
-    void parseMultiResult01()
+    public void parseMultiResult01()
             throws SearchException, IOException, CredentialsException, StorageException {
-        setLocale(searchEngine.getLocale(context));
+
         final String locationHeader = "https://www.bol.com/be/nl/s/?searchtext=+9789056478193+";
-        final String filename = "/bol/multi-1-result-9789056478193.html";
+        final int resId = com.hardbacknutter.nevertoomanybooks.test
+                .R.raw.bol_multi_1_result_9789056478193;
 
         final RealNumberParser realNumberParser =
                 new RealNumberParser(List.of(searchEngine.getLocale(context)));
 
-        final Document document = loadDocument(filename, UTF_8, locationHeader);
+        final Document document = loadDocument(resId, UTF_8, locationHeader);
+        final Book book = new Book();
         searchEngine.parseMultiResult(context, document, new boolean[]{false, false}, book);
-        // System.out.println(book);
+        // Log.d(TAG, book.toString());
 
         assertEquals("nijntjes voorleesfeest", book.getString(DBKey.TITLE, null));
         assertEquals("9789056478193", book.getString(DBKey.BOOK_ISBN, null));
@@ -109,18 +112,20 @@ public class BolTest
 
     /** Network access! */
     @Test
-    void parseMultiResult02()
+    public void parseMultiResult02()
             throws SearchException, IOException, CredentialsException, StorageException {
-        setLocale(searchEngine.getLocale(context));
+
         final String locationHeader = "https://www.bol.com/be/nl/s/?searchtext=asimov%20foundation&suggestFragment=asimov";
-        final String filename = "/bol/asimov_foundation.html";
+        final int resId = com.hardbacknutter.nevertoomanybooks.test
+                .R.raw.bol_asimov_foundation;
 
         final RealNumberParser realNumberParser =
                 new RealNumberParser(List.of(searchEngine.getLocale(context)));
 
-        final Document document = loadDocument(filename, UTF_8, locationHeader);
+        final Document document = loadDocument(resId, UTF_8, locationHeader);
+        final Book book = new Book();
         searchEngine.parseMultiResult(context, document, new boolean[]{true, true}, book);
-        //System.out.println(book);
+        // Log.d(TAG, book.toString());
 
         assertEquals("Foundation Trilogy", book.getString(DBKey.TITLE, null));
         assertEquals("9781841593326", book.getString(DBKey.BOOK_ISBN, null));
@@ -169,19 +174,21 @@ public class BolTest
      * be/nl + dutch book
      */
     @Test
-    void parse01()
+    public void parse01()
             throws SearchException, IOException, CredentialsException, StorageException {
-        setLocale(searchEngine.getLocale(context));
+
         final String locationHeader =
                 "https://www.bol.com/be/nl/p/alter-ego/9300000135231911/?s2a=";
-        final String filename = "/bol/9789044652901-be-nl-dutch.html";
+        final int resId = com.hardbacknutter.nevertoomanybooks.test
+                .R.raw.bol_9789044652901_be_nl_dutch;
 
         final RealNumberParser realNumberParser =
                 new RealNumberParser(List.of(searchEngine.getLocale(context)));
 
-        final Document document = loadDocument(filename, UTF_8, locationHeader);
+        final Document document = loadDocument(resId, UTF_8, locationHeader);
+        final Book book = new Book();
         searchEngine.parse(context, document, new boolean[]{true, true}, book);
-        //System.out.println(book);
+        // Log.d(TAG, book.toString());
 
         assertEquals("Alter Ego", book.getString(DBKey.TITLE, null));
         assertEquals("9789044652901", book.getString(DBKey.BOOK_ISBN, null));
@@ -224,19 +231,21 @@ public class BolTest
      * be/fr + dutch book
      */
     @Test
-    void parse01fr()
+    public void parse01fr()
             throws SearchException, IOException, CredentialsException, StorageException {
-        setLocale(searchEngine.getLocale(context));
+
         final String locationHeader =
                 "https://www.bol.com/be/fr/p/alter-ego/9300000135231911/?s2a=";
-        final String filename = "/bol/9789044652901-nl-fr.html";
+        final int resId = com.hardbacknutter.nevertoomanybooks.test
+                .R.raw.bol_9789044652901_nl_fr;
 
         final RealNumberParser realNumberParser =
                 new RealNumberParser(List.of(searchEngine.getLocale(context)));
 
-        final Document document = loadDocument(filename, UTF_8, locationHeader);
+        final Document document = loadDocument(resId, UTF_8, locationHeader);
+        final Book book = new Book();
         searchEngine.parse(context, document, new boolean[]{false, false}, book);
-        //System.out.println(book);
+        // Log.d(TAG, book.toString());
         // there won't be a title!
         assertNotEquals("Alter Ego", book.getString(DBKey.TITLE, null));
 //        assertEquals("9789044652901", book.getString(DBKey.BOOK_ISBN, null));
@@ -262,18 +271,20 @@ public class BolTest
     }
 
     @Test
-    void parse02()
+    public void parse02()
             throws SearchException, IOException, CredentialsException, StorageException {
-        setLocale(searchEngine.getLocale(context));
+
         final String locationHeader = "https://www.bol.com/be/nl/p/europa/9300000130411439/?promo=main_861_new_for_you___product_0_9300000130411439&bltgh=vwTKjOiKpqSmgLkGxPZJow.90_91.92.ProductImage";
-        final String filename = "/bol/9789044544725.html";
+        final int resId = com.hardbacknutter.nevertoomanybooks.test
+                .R.raw.bol_9789044544725;
 
         final RealNumberParser realNumberParser =
                 new RealNumberParser(List.of(searchEngine.getLocale(context)));
 
-        final Document document = loadDocument(filename, UTF_8, locationHeader);
+        final Document document = loadDocument(resId, UTF_8, locationHeader);
+        final Book book = new Book();
         searchEngine.parse(context, document, new boolean[]{true, true}, book);
-        //System.out.println(book);
+        // Log.d(TAG, book.toString());
 
         assertEquals("Europa", book.getString(DBKey.TITLE, null));
         assertEquals("9789044544725", book.getString(DBKey.BOOK_ISBN, null));
@@ -320,19 +331,21 @@ public class BolTest
 
     /** The redirect from {@link #parseMultiResult01()} */
     @Test
-    void parse03()
+    public void parse03()
             throws SearchException, IOException, CredentialsException, StorageException {
-        setLocale(searchEngine.getLocale(context));
+
         final String locationHeader =
                 "https://www.bol.com/be/nl/p/nijntjes-voorleesfeest/9200000122271922/";
-        final String filename = "/bol/9789056478193.html";
+        final int resId = com.hardbacknutter.nevertoomanybooks.test
+                .R.raw.bol_9789056478193;
 
         final RealNumberParser realNumberParser =
                 new RealNumberParser(List.of(searchEngine.getLocale(context)));
 
-        final Document document = loadDocument(filename, UTF_8, locationHeader);
+        final Document document = loadDocument(resId, UTF_8, locationHeader);
+        final Book book = new Book();
         searchEngine.parse(context, document, new boolean[]{true, true}, book);
-        //System.out.println(book);
+        // Log.d(TAG, book.toString());
 
         assertEquals("nijntjes voorleesfeest", book.getString(DBKey.TITLE, null));
         assertEquals("9789056478193", book.getString(DBKey.BOOK_ISBN, null));
@@ -361,19 +374,21 @@ public class BolTest
 
     /** The redirect from {@link #parseMultiResult02()} */
     @Test
-    void parse04()
+    public void parse04()
             throws SearchException, IOException, CredentialsException, StorageException {
-        setLocale(searchEngine.getLocale(context));
+
         final String locationHeader =
                 "https://www.bol.com/be/nl/p/foundation-trilogy/1001004009994645/";
-        final String filename = "/bol/9781841593326.html";
+        final int resId = com.hardbacknutter.nevertoomanybooks.test
+                .R.raw.bol_9781841593326;
 
         final RealNumberParser realNumberParser =
                 new RealNumberParser(List.of(searchEngine.getLocale(context)));
 
-        final Document document = loadDocument(filename, UTF_8, locationHeader);
+        final Document document = loadDocument(resId, UTF_8, locationHeader);
+        final Book book = new Book();
         searchEngine.parse(context, document, new boolean[]{true, true}, book);
-        // System.out.println(book);
+        // Log.d(TAG, book.toString());
 
         assertEquals("Foundation Trilogy", book.getString(DBKey.TITLE, null));
         assertEquals("9781841593326", book.getString(DBKey.BOOK_ISBN, null));
