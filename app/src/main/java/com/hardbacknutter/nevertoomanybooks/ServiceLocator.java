50714cfbbb8550c5f633a515f99d54c470d77d6b
hardbacknutter <user@localhost>
2024-06-08 12:51:05 +0100

    add getters for the Theme - hardcoded for now

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/ServiceLocator.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/ServiceLocator.java
@@ -26,6 +26,7 @@ import android.os.LocaleList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StyleRes;
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.PreferenceManager;
 
@@ -307,6 +308,16 @@ public class ServiceLocator {
     @NonNull
     public Context getLocalizedAppContext() {
         return getAppLocale().apply(sInstance.appContext);
+    }
+
+    @StyleRes
+    public int getThemeResId() {
+        return R.style.Theme_App;
+    }
+
+    @StyleRes
+    public int getFullScreenThemeResId() {
+        return R.style.Theme_App_FullScreen;
     }
 
     /**
