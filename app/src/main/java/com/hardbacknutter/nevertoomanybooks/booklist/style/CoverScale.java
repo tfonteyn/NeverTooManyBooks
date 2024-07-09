c1075440451a7ee3ae0075e275ba05c3674f03c5
hardbacknutter <user@localhost>
2024-07-09 14:28:52 +0100

    Android 15: use WindowMetricsCalculator

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/booklist/style/CoverScale.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/booklist/style/CoverScale.java
@@ -1,5 +1,5 @@
 /*
- * @Copyright 2018-2023 HardBackNutter
+ * @Copyright 2018-2024 HardBackNutter
  * @License GNU General Public License
  *
  * This file is part of NeverTooManyBooks.
@@ -30,6 +30,7 @@ import androidx.annotation.Dimension;
 import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
 import androidx.core.math.MathUtils;
+import androidx.window.layout.WindowMetricsCalculator;
 
 import java.util.Arrays;
 
@@ -40,15 +41,6 @@ import com.hardbacknutter.nevertoomanybooks.R;
  * <strong>Never change the 'scale' values</strong>, they get stored in the db.
  * <p>
  * These values are used as the index into a resource array.
- * <p>
- * FIXME: our use of {@link Configuration#screenWidthDp} will break on dual-screen setups
- * (but who can afford those ...)
- * From {@link Configuration} docs:
- * <pre>
- *     if the app is spanning both screens of a dual-screen device
- *     (with the screens side by side), {@code screenWidthDp} represents the
- *     width of both screens
- * </pre>
  */
 public enum CoverScale {
     /** Don't show the cover at all. */
@@ -60,13 +52,17 @@ public enum CoverScale {
      */
     Medium(2),
     Large(3),
+
     /**
      * Represented to the user (in preferences screen) as X-large.
-     * In BoB grid-mode this will:
-     * - portrait: 1 image fills up the entire screen width.
-     * - landscape: 2 images side by side fills up the entire screen width
+     * <p>
+     * In {@link Style.Layout#Grid} mode this will result in:
+     * <ul>
+     *     <li>portrait : 1 image fills up the entire screen width.</li>
+     *     <li>landscape: 2 images side by side fills up the entire screen width</li>
+     * </ul>
      */
-    MAX(4);
+    Maximum(4);
 
     public static final CoverScale DEFAULT = Medium;
     /**
@@ -85,8 +81,8 @@ public enum CoverScale {
 
     @NonNull
     static CoverScale byId(final int id) {
-        if (id > MAX.scale) {
-            return MAX;
+        if (id > Maximum.scale) {
+            return Maximum;
         } else if (id < Hidden.scale) {
             return Hidden;
         } else {
@@ -94,19 +90,43 @@ public enum CoverScale {
         }
     }
 
+    @Dimension
+    private static int getWindowWidthInPx(@NonNull final Context context) {
+        return WindowMetricsCalculator
+                .getOrCreate()
+                .computeCurrentWindowMetrics(context)
+                .getBounds()
+                .width();
+    }
+
+    /**
+     * Get the numerical value of this scale.
+     *
+     * @return numerical value for storing in database etc...
+     */
     public int getScale() {
         return scale;
     }
 
+    /**
+     * Increase and get the new scale.
+     *
+     * @return the next larger scale
+     */
     @NonNull
     public CoverScale larger() {
-        final int next = MathUtils.clamp(scale + 1, Small.scale, MAX.scale);
+        final int next = MathUtils.clamp(scale + 1, Small.scale, Maximum.scale);
         return values()[next];
     }
 
+    /**
+     * Decrease and get the new scale.
+     *
+     * @return the next smaller scale
+     */
     @NonNull
     public CoverScale smaller() {
-        final int next = MathUtils.clamp(scale - 1, Small.scale, MAX.scale);
+        final int next = MathUtils.clamp(scale - 1, Small.scale, Maximum.scale);
         return values()[next];
     }
 
@@ -124,19 +144,17 @@ public enum CoverScale {
         if (this == Hidden) {
             return 0;
         }
-        if (this == MAX && layout == Style.Layout.Grid) {
-            final Resources res = context.getResources();
-            // Calculate depending on the available screen width.
-            final double screenWidthInPx = Math.floor(
-                    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                                              res.getConfiguration().screenWidthDp,
-                                              res.getDisplayMetrics()));
-            if (res.getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+        if (this == Maximum && layout == Style.Layout.Grid) {
+            final int windowWidthInPx = getWindowWidthInPx(context);
+
+            if (context.getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE) {
                 // In landscape, half.
-                return (int) (screenWidthInPx / 2);
+                return windowWidthInPx / 2;
             } else {
                 // In portrait, the entire screen width
-                return (int) screenWidthInPx;
+                return windowWidthInPx;
             }
         }
 
@@ -162,9 +180,8 @@ public enum CoverScale {
     public int getGridSpanCount(@NonNull final Context context) {
         final Resources res = context.getResources();
 
-        if (this == MAX) {
-            if (res.getConfiguration().orientation
-                == Configuration.ORIENTATION_LANDSCAPE) {
+        if (this == Maximum) {
+            if (res.getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 return 2;
             } else {
                 // Configuration.ORIENTATION_PORTRAIT
@@ -174,13 +191,19 @@ public enum CoverScale {
             // Calculate depending on the available screen width.
             final TypedArray coverSizes = res.obtainTypedArray(R.array.cover_max_width);
             try {
-                final int screenWidthDp = res.getConfiguration().screenWidthDp;
-                // Multiply by 0.6 as the values in the resource are optimized for list-mode,
-                // where we aim to fill up 1/3 of the width with the image, and 2/3 with text.
+                // Multiply the cover-width by 0.6 as the values in the resource are
+                // optimized for list-mode where we aim to fill up 1/3 of the width
+                // with the image, and 2/3 with text.
                 // The 0.6 could likely be tuned on a screen size basis... but the differences
                 // will be minimal hence not bothering for now.
-                final float coverWidthDp = 0.6f * coverSizes.getDimension(scale, 1);
-                return (int) Math.floor(screenWidthDp / coverWidthDp);
+                final float coverWidthPx = 0.6f * TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        // The value in dp
+                        coverSizes.getDimension(scale, 1),
+                        res.getDisplayMetrics());
+
+                return (int) Math.floor(getWindowWidthInPx(context) / coverWidthPx);
+
             } finally {
                 coverSizes.recycle();
             }
@@ -188,5 +211,4 @@ public enum CoverScale {
         // Hidden.... we should never get here... flw
         return 1;
     }
-
 }
