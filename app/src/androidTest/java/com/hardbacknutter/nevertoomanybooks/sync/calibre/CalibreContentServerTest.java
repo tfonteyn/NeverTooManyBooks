16d73dc1bee3b6feb4a79bf05e5259ad051058e9
hardbacknutter <user@localhost>
2024-11-16 11:19:34 +0000

    context usage changes

--- app/src/androidTest/java/com/hardbacknutter/nevertoomanybooks/sync/calibre/CalibreContentServerTest.java
+++ app/src/androidTest/java/com/hardbacknutter/nevertoomanybooks/sync/calibre/CalibreContentServerTest.java
@@ -1,5 +1,5 @@
 /*
- * @Copyright 2018-2023 HardBackNutter
+ * @Copyright 2018-2024 HardBackNutter
  * @License GNU General Public License
  *
  * This file is part of NeverTooManyBooks.
@@ -136,6 +136,6 @@ public class CalibreContentServerTest {
     public void connection()
             throws IOException, StorageException {
 
-        assertTrue(server.validateConnection());
+        assertTrue(server.validateConnection(context));
     }
 }
