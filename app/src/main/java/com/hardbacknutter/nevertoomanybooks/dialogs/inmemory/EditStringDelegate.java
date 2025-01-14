ba81df6bb8a543362fdaec70e07f30d733b5c951
hardbacknutter <user@localhost>
2025-01-14 23:01:44 +0000

    add a Flex dialog for editing a generic String

---
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/dialogs/inmemory/EditStringDelegate.java
@@ -0,0 +1,163 @@
+/*
+ * @Copyright 2018-2025 HardBackNutter
+ * @License GNU General Public License
+ *
+ * This file is part of NeverTooManyBooks.
+ *
+ * NeverTooManyBooks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeverTooManyBooks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hardbacknutter.nevertoomanybooks.dialogs.inmemory;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.Objects;
+
+import com.hardbacknutter.nevertoomanybooks.R;
+import com.hardbacknutter.nevertoomanybooks.databinding.DialogEditStringContentBinding;
+import com.hardbacknutter.nevertoomanybooks.dialogs.DialogLauncher;
+import com.hardbacknutter.nevertoomanybooks.dialogs.DialogType;
+import com.hardbacknutter.nevertoomanybooks.dialogs.FlexDialogDelegate;
+
+public class EditStringDelegate
+        implements FlexDialogDelegate {
+
+    @NonNull
+    private final DialogFragment owner;
+    @NonNull
+    private final String requestKey;
+    @NonNull
+    private final String dialogTitle;
+    @Nullable
+    private final String dialogMessage;
+
+    private final EditStringViewModel vm;
+    private DialogEditStringContentBinding vb;
+
+    @Nullable
+    private Toolbar toolbar;
+
+    EditStringDelegate(@NonNull final DialogFragment owner,
+                       @NonNull final Bundle args) {
+        this.owner = owner;
+        requestKey = Objects.requireNonNull(args.getString(DialogLauncher.BKEY_REQUEST_KEY),
+                                            DialogLauncher.BKEY_REQUEST_KEY);
+        dialogTitle = args.getString(PartialDatePickerLauncher.BKEY_DIALOG_TITLE,
+                                     owner.getString(R.string.action_edit));
+        dialogMessage = args.getString(PartialDatePickerLauncher.BKEY_DIALOG_MESSAGE, null);
+
+        vm = new ViewModelProvider(owner).get(EditStringViewModel.class);
+        vm.init(args);
+    }
+
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater,
+                             @Nullable final ViewGroup container) {
+        // Ensure components match current Locale order BEFORE we bind the views.
+        final View view = inflater.inflate(R.layout.dialog_edit_string_content,
+                                           container, false);
+        vb = DialogEditStringContentBinding.bind(view);
+        return vb.getRoot();
+    }
+
+    @Override
+    @NonNull
+    public View onCreateFullscreen(@NonNull final LayoutInflater inflater,
+                                   @Nullable final ViewGroup container) {
+        final View view = inflater.inflate(R.layout.dialog_edit_string, container, false);
+
+        vb = DialogEditStringContentBinding.bind(view.findViewById(R.id.dialog_content));
+        return view;
+    }
+
+    @NonNull
+    public Toolbar getToolbar() {
+        return Objects.requireNonNull(toolbar, "No toolbar set");
+    }
+
+    @Override
+    public void setToolbar(@Nullable final Toolbar toolbar) {
+        this.toolbar = toolbar;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final DialogType dialogType) {
+        if (toolbar != null) {
+            if (dialogType == DialogType.BottomSheet) {
+                toolbar.inflateMenu(R.menu.toolbar_action_save);
+            }
+            initToolbar(owner, dialogType, toolbar);
+            toolbar.setTitle(dialogTitle);
+        }
+
+        if (dialogMessage != null && !dialogMessage.isEmpty()) {
+            vb.message.setText(dialogMessage);
+            vb.message.setVisibility(View.VISIBLE);
+        } else {
+            vb.message.setVisibility(View.GONE);
+        }
+
+        vb.editString.setText(vm.getCurrentValue());
+        vb.editString.requestFocus();
+    }
+
+    @Override
+    public void onToolbarNavigationClick(@NonNull final View v) {
+        owner.dismiss();
+    }
+
+    @Override
+    public boolean onToolbarButtonClick(@Nullable final View button) {
+        if (button != null) {
+            final int id = button.getId();
+            if (id == R.id.toolbar_btn_save || id == R.id.btn_positive) {
+                if (saveChanges()) {
+                    owner.dismiss();
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onPause(@NonNull final LifecycleOwner lifecycleOwner) {
+        viewToModel();
+    }
+
+    private boolean saveChanges() {
+        viewToModel();
+
+        EditStringLauncher.setResult(owner, requestKey,
+                                     vm.getPreviousValue(),
+                                     vm.getCurrentValue(),
+                                     vm.getExtras());
+        return true;
+    }
+
+    private void viewToModel() {
+        vm.setCurrentValue(vb.editString.getText().toString().trim());
+    }
+}
---
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/dialogs/inmemory/EditStringViewModel.java
@@ -0,0 +1,76 @@
+/*
+ * @Copyright 2018-2025 HardBackNutter
+ * @License GNU General Public License
+ *
+ * This file is part of NeverTooManyBooks.
+ *
+ * NeverTooManyBooks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeverTooManyBooks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hardbacknutter.nevertoomanybooks.dialogs.inmemory;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+
+public class EditStringViewModel
+        extends ViewModel {
+
+    @NonNull
+    private String previousValue = "";
+    @NonNull
+    private String currentValue = "";
+    @Nullable
+    private Bundle extras;
+
+    private boolean initDone;
+
+    /**
+     * Pseudo constructor.
+     *
+     * @param args {@link Fragment#requireArguments()}
+     */
+    void init(@NonNull final Bundle args) {
+        if (!initDone) {
+            initDone = true;
+
+            currentValue = args.getString(EditStringLauncher.BKEY_CURRENT, "");
+            previousValue = currentValue;
+
+            extras = args.getBundle(AutoCompletePickerLauncher.BKEY_EXTRAS);
+        }
+    }
+
+    @NonNull
+    String getPreviousValue() {
+        return previousValue;
+    }
+
+    @NonNull
+    String getCurrentValue() {
+        return currentValue;
+    }
+
+    void setCurrentValue(@NonNull final String currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    @Nullable
+    Bundle getExtras() {
+        return extras;
+    }
+}
