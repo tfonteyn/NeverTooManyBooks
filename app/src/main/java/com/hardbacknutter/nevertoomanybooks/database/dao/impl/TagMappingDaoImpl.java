306321bff92e0769fe30db1ab74ae12b2de4f7b6
hardbacknutter <user@localhost>
2025-02-02 09:55:05 +0000

    remove limited use examples

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/database/dao/impl/TagMappingDaoImpl.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/database/dao/impl/TagMappingDaoImpl.java
@@ -78,11 +78,7 @@ public class TagMappingDaoImpl
                 new Pair<>("Sciencefiction", Set.of("Science Fiction")),
                 // splitting of combinations into multiple tags
                 new Pair<>("science fiction fantasy", Set.of("Science Fiction", "Fantasy")),
-                new Pair<>("Science Fiction & Fantasy", Set.of("Science Fiction", "Fantasy")),
-                // diacritic misspellings
-                new Pair<>("bandes-dessinees", Set.of("bandes-dessinées")),
-                new Pair<>("bande dessinee", Set.of("bandes-dessinées")),
-                new Pair<>("bande dessinée", Set.of("bandes-dessinées"))
+                new Pair<>("Science Fiction & Fantasy", Set.of("Science Fiction", "Fantasy"))
                 // and so on... up to the user to setup theirs obviously
         );
     }
