9e1ad03b14302d3f3a079013d24b5ebd522dcfe6
hardbacknutter <user@localhost>
2025-02-14 22:13:01 +0000

    try to guess when the original language might be english

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/kbnl/KbNlBookHandler.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/kbnl/KbNlBookHandler.java
@@ -23,6 +23,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -297,9 +298,14 @@ class KbNlBookHandler
 
         final Matcher matcher = TRANSLATION_PATTERN.matcher(data);
         if (matcher.find()) {
-            final String origTitle = matcher.group(1);
-            if (origTitle != null && !origTitle.isEmpty()) {
-                book.putString(DBKey.TRANSLATION_ORIGINAL_TITLE, origTitle);
+            final String originalTitle = matcher.group(1);
+            if (originalTitle != null && !originalTitle.isEmpty()) {
+                book.putString(DBKey.TRANSLATION_ORIGINAL_TITLE, originalTitle);
+                // another wild attempt...
+                final String lc = originalTitle.toLowerCase(Locale.ENGLISH);
+                if (lc.startsWith("the ") || lc.contains(" the ")) {
+                    book.putString(DBKey.TRANSLATION_ORIGINAL_LANGUAGE, "eng");
+                }
             }
         }
     }
