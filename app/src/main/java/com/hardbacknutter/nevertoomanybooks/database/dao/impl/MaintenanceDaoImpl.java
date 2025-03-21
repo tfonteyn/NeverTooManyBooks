186d4bd18e517596bc75896c7c823f376f16d400
hardbacknutter <user@localhost>
2025-03-21 18:07:10 +0000

    use SynchronizedStatement + optimize sql compile/use loops

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/database/dao/impl/MaintenanceDaoImpl.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/database/dao/impl/MaintenanceDaoImpl.java
@@ -19,7 +19,6 @@
  */
 package com.hardbacknutter.nevertoomanybooks.database.dao.impl;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 
@@ -30,12 +29,10 @@ import java.util.List;
 import java.util.Locale;
 import java.util.function.Supplier;
 
-import com.hardbacknutter.nevertoomanybooks.BuildConfig;
 import com.hardbacknutter.nevertoomanybooks.core.database.SqlEncode;
 import com.hardbacknutter.nevertoomanybooks.core.database.SynchronizedDb;
+import com.hardbacknutter.nevertoomanybooks.core.database.SynchronizedStatement;
 import com.hardbacknutter.nevertoomanybooks.core.database.Synchronizer;
-import com.hardbacknutter.nevertoomanybooks.core.database.TableDefinition;
-import com.hardbacknutter.nevertoomanybooks.core.database.TransactionException;
 import com.hardbacknutter.nevertoomanybooks.core.utils.LocaleListUtils;
 import com.hardbacknutter.nevertoomanybooks.database.DBKey;
 import com.hardbacknutter.nevertoomanybooks.database.dao.AuthorDao;
@@ -60,40 +57,45 @@ public class MaintenanceDaoImpl
     /** Log tag. */
     private static final String TAG = "MaintenanceDaoImpl";
 
-    /** All Series for a rebuild of the {@link DBKey.SERIES#TITLE_OB} column. */
-    private static final String SELECT_SERIES_FOR_ORDER_BY_REBUILD =
-            // The index of PK_ID, SERIES_TITLE, SERIES_TITLE_OB is hardcoded
-            // Don't change!
-            SELECT_ + DBKey.PK_ID
-            + ',' + DBKey.SERIES.TITLE
-            + ',' + DBKey.SERIES.TITLE_OB
-            + _FROM_ + TBL_SERIES.getName();
-
-    /** All Publishers for a rebuild of the {@link DBKey.PUBLISHER#NAME_OB} column. */
-    private static final String SELECT_PUBLISHERS_FOR_ORDER_BY_REBUILD =
-            // The index of PK_ID, PUBLISHER_NAME, PUBLISHER_NAME_OB is hardcoded
-            // Don't change!
-            SELECT_ + DBKey.PK_ID
-            + ',' + DBKey.PUBLISHER.NAME
-            + ',' + DBKey.PUBLISHER.NAME_OB
-            + _FROM_ + TBL_PUBLISHERS.getName();
-
     /** All Book titles for a rebuild of the {@link DBKey#TITLE_OB} column. */
     private static final String BOOK_TITLES =
-            // The index of PK_ID, TITLE, TITLE_OB is hardcoded - don't change!
             SELECT_ + DBKey.PK_ID
             + ',' + DBKey.TITLE
             + ',' + DBKey.TITLE_OB
             + ',' + DBKey.LANGUAGE
             + _FROM_ + TBL_BOOKS.getName();
+    /** All Series for a rebuild of the {@link DBKey.SERIES#TITLE_OB} column. */
+    private static final String SERIES_TITLES =
+            SELECT_ + DBKey.PK_ID
+            + ',' + DBKey.SERIES.TITLE
+            + ',' + DBKey.SERIES.TITLE_OB
+            + _FROM_ + TBL_SERIES.getName();
+    /** All Publishers for a rebuild of the {@link DBKey.PUBLISHER#NAME_OB} column. */
+    private static final String PUBLISHERS_NAMES =
+            SELECT_ + DBKey.PK_ID
+            + ',' + DBKey.PUBLISHER.NAME
+            + ',' + DBKey.PUBLISHER.NAME_OB
+            + _FROM_ + TBL_PUBLISHERS.getName();
 
     /** All TocEntry titles for a rebuild of the {@link DBKey#TITLE_OB} column. */
     private static final String TOC_ENTRY_TITLES =
-            // The index of PK_ID, TITLE, TITLE_OB is hardcoded - don't change!
             SELECT_ + DBKey.PK_ID
             + ',' + DBKey.TITLE
             + ',' + DBKey.TITLE_OB
             + _FROM_ + TBL_TOC_ENTRIES.getName();
+
+    private static final String BOOK_REBUILD =
+            UPDATE_ + TBL_BOOKS.getName() + _SET_ + DBKey.TITLE_OB + "=?"
+            + _WHERE_ + DBKey.PK_ID + "=?";
+    private static final String SERIES_REBUILD =
+            UPDATE_ + TBL_SERIES.getName() + _SET_ + DBKey.SERIES.TITLE_OB + "=?"
+            + _WHERE_ + DBKey.PK_ID + "=?";
+    private static final String PUBLISHERS_REBUILD =
+            UPDATE_ + TBL_PUBLISHERS.getName() + _SET_ + DBKey.PUBLISHER.NAME_OB + "=?"
+            + _WHERE_ + DBKey.PK_ID + "=?";
+    private static final String TOC_REBUILD =
+            UPDATE_ + TBL_TOC_ENTRIES.getName() + _SET_ + DBKey.TITLE_OB + "=?"
+            + _WHERE_ + DBKey.PK_ID + "=?";
 
     @NonNull
     private final Supplier<AuthorDao> authorDaoSupplier;
@@ -115,7 +117,7 @@ public class MaintenanceDaoImpl
      * @param authorDaoSupplier     deferred supplier for the {@link AuthorDao}
      * @param seriesDaoSupplier     deferred supplier for the {@link SeriesDao}
      * @param publisherDaoSupplier  deferred supplier for the {@link PublisherDao}
-     * @param tocEntryDaoSupplier  deferred supplier for the {@link TocEntryDao}
+     * @param tocEntryDaoSupplier   deferred supplier for the {@link TocEntryDao}
      * @param appLocaleSupplier     deferred supplier for the {@link AppLocale}
      * @param reorderHelperSupplier deferred supplier for the {@link ReorderHelper}
      */
@@ -165,50 +167,93 @@ public class MaintenanceDaoImpl
         final Locale userLocale = context.getResources().getConfiguration().getLocales().get(0);
         final List<Locale> locales = LocaleListUtils.asList(context);
 
-        // Books
-        String language;
-        Locale bookLocale;
-
         Synchronizer.SyncLock txLock = null;
         try {
             if (!db.inTransaction()) {
                 txLock = db.beginTransaction(true);
             }
 
-            try (Cursor cursor = db.rawQuery(BOOK_TITLES, null)) {
-                final int langIdx = cursor.getColumnIndex(DBKey.LANGUAGE);
+            try (Cursor cursor = db.rawQuery(BOOK_TITLES, null);
+                 SynchronizedStatement stmt = db.compileStatement(BOOK_REBUILD)) {
                 while (cursor.moveToNext()) {
-                    language = cursor.getString(langIdx);
-                    bookLocale = appLocaleSupplier.get().getLocale(context, language)
-                                                  .orElse(userLocale);
-                    rebuildOrderByTitleColumns(context, bookLocale, locales,
-                                               cursor, TBL_BOOKS, DBKey.TITLE_OB);
+                    final long id = cursor.getLong(0);
+                    final String title = cursor.getString(1);
+                    final String currentObTitle = cursor.getString(2);
+                    final Locale bookLocale = appLocaleSupplier
+                            .get()
+                            .getLocale(context, cursor.getString(3))
+                            .orElse(userLocale);
+                    final String rebuildObTitle = reorderHelperSupplier
+                            .get().reorderForSorting(context, title, bookLocale, locales);
+
+                    // only update the database if actually needed.
+                    if (!currentObTitle.equals(rebuildObTitle)) {
+                        stmt.bindString(1, SqlEncode.orderByColumn(rebuildObTitle, bookLocale));
+                        stmt.bindLong(2, id);
+                        stmt.executeUpdateDelete();
+                    }
                 }
             }
 
             // We should use the locale from the 1st book in the series...
             // but that is a huge overhead so we use the user-locale directly.
-            try (Cursor cursor = db.rawQuery(SELECT_SERIES_FOR_ORDER_BY_REBUILD, null)) {
+            try (Cursor cursor = db.rawQuery(SERIES_TITLES, null);
+                 SynchronizedStatement stmt = db.compileStatement(SERIES_REBUILD)) {
                 while (cursor.moveToNext()) {
-                    rebuildOrderByTitleColumns(context, userLocale, locales,
-                                               cursor, TBL_SERIES, DBKey.SERIES.TITLE_OB);
+                    final long id = cursor.getLong(0);
+                    final String title = cursor.getString(1);
+                    final String currentObTitle = cursor.getString(2);
+
+                    final String rebuildObTitle = reorderHelperSupplier
+                            .get().reorderForSorting(context, title, userLocale, locales);
+
+                    // only update the database if actually needed.
+                    if (!currentObTitle.equals(rebuildObTitle)) {
+                        stmt.bindString(1, SqlEncode.orderByColumn(rebuildObTitle, userLocale));
+                        stmt.bindLong(2, id);
+                        stmt.executeUpdateDelete();
+                    }
                 }
             }
 
             // A publisher is not linked to a Locale, so we use the user-locale directly.
-            try (Cursor cursor = db.rawQuery(SELECT_PUBLISHERS_FOR_ORDER_BY_REBUILD, null)) {
+            try (Cursor cursor = db.rawQuery(PUBLISHERS_NAMES, null);
+                 SynchronizedStatement stmt = db.compileStatement(PUBLISHERS_REBUILD)) {
                 while (cursor.moveToNext()) {
-                    rebuildOrderByTitleColumns(context, userLocale, locales,
-                                               cursor, TBL_PUBLISHERS, DBKey.PUBLISHER.NAME_OB);
+                    final long id = cursor.getLong(0);
+                    final String title = cursor.getString(1);
+                    final String currentObTitle = cursor.getString(2);
+
+                    final String rebuildObTitle = reorderHelperSupplier
+                            .get().reorderForSorting(context, title, userLocale, locales);
+
+                    // only update the database if actually needed.
+                    if (!currentObTitle.equals(rebuildObTitle)) {
+                        stmt.bindString(1, SqlEncode.orderByColumn(rebuildObTitle, userLocale));
+                        stmt.bindLong(2, id);
+                        stmt.executeUpdateDelete();
+                    }
                 }
             }
 
             // We should use primary book or Author Locale...
             // but that is a huge overhead, so we use the user-locale directly.
-            try (Cursor cursor = db.rawQuery(TOC_ENTRY_TITLES, null)) {
+            try (Cursor cursor = db.rawQuery(TOC_ENTRY_TITLES, null);
+                 SynchronizedStatement stmt = db.compileStatement(TOC_REBUILD)) {
                 while (cursor.moveToNext()) {
-                    rebuildOrderByTitleColumns(context, userLocale, locales,
-                                               cursor, TBL_TOC_ENTRIES, DBKey.TITLE_OB);
+                    final long id = cursor.getLong(0);
+                    final String title = cursor.getString(1);
+                    final String currentObTitle = cursor.getString(2);
+
+                    final String rebuildObTitle = reorderHelperSupplier
+                            .get().reorderForSorting(context, title, userLocale, locales);
+
+                    // only update the database if actually needed.
+                    if (!currentObTitle.equals(rebuildObTitle)) {
+                        stmt.bindString(1, SqlEncode.orderByColumn(rebuildObTitle, userLocale));
+                        stmt.bindLong(2, id);
+                        stmt.executeUpdateDelete();
+                    }
                 }
             }
 
@@ -219,47 +264,6 @@ public class MaintenanceDaoImpl
             if (txLock != null) {
                 db.endTransaction(txLock);
             }
-        }
-    }
-
-    /**
-     * Process a <strong>single row</strong> from the cursor.
-     *
-     * @param context    Current context
-     * @param locale     the locale of the title
-     * @param locales    all user locales to use for parsing
-     * @param cursor     positioned on the row to handle
-     * @param table      to update
-     * @param domainName to update
-     */
-    @WorkerThread
-    private void rebuildOrderByTitleColumns(@NonNull final Context context,
-                                            @NonNull final Locale locale,
-                                            @NonNull final List<Locale> locales,
-                                            @NonNull final Cursor cursor,
-                                            @NonNull final TableDefinition table,
-                                            @NonNull final String domainName) {
-
-        if (BuildConfig.DEBUG /* always */) {
-            if (!db.inTransaction()) {
-                //noinspection CheckStyle
-                throw new TransactionException(TransactionException.REQUIRED);
-            }
-        }
-
-        final long id = cursor.getLong(0);
-        final String title = cursor.getString(1);
-        final String currentObTitle = cursor.getString(2);
-
-        final String rebuildObTitle = reorderHelperSupplier
-                .get().reorderForSorting(context, title, locale, locales);
-
-        // only update the database if actually needed.
-        if (!currentObTitle.equals(rebuildObTitle)) {
-            final ContentValues cv = new ContentValues();
-            cv.put(domainName, SqlEncode.orderByColumn(rebuildObTitle, locale));
-            db.update(table.getName(), cv, DBKey.PK_ID + "=?",
-                      new String[]{String.valueOf(id)});
         }
     }
 }
