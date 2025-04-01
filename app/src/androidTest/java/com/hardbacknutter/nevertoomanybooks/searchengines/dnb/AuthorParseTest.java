00478857eb503107e6a5ddd8ca3f5354b2354c68
hardbacknutter <user@localhost>
2025-04-01 18:42:27 +0100

    add DnbAuthorResolver with caveats

---
+++ app/src/androidTest/java/com/hardbacknutter/nevertoomanybooks/searchengines/dnb/AuthorParseTest.java
@@ -0,0 +1,145 @@
+/*
+ * @Copyright 2018-2025 HardBackNutter
+ * @License GNU General Public License
+ *
+ * This file is part of NeverTooManyBooks.
+ *
+ * NeverTooManyBooks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeverTooManyBooks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hardbacknutter.nevertoomanybooks.searchengines.dnb;
+
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import com.hardbacknutter.nevertoomanybooks.BaseDBTest;
+import com.hardbacknutter.nevertoomanybooks.TestProgressListener;
+import com.hardbacknutter.nevertoomanybooks.core.database.DaoWriteException;
+import com.hardbacknutter.nevertoomanybooks.core.network.CredentialsException;
+import com.hardbacknutter.nevertoomanybooks.core.storage.StorageException;
+import com.hardbacknutter.nevertoomanybooks.entities.Author;
+import com.hardbacknutter.nevertoomanybooks.entities.Identifier;
+import com.hardbacknutter.nevertoomanybooks.searchengines.EngineId;
+import com.hardbacknutter.nevertoomanybooks.searchengines.SearchException;
+import com.hardbacknutter.nevertoomanybooks.utils.AppLocale;
+
+import org.jsoup.nodes.Document;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class AuthorParseTest
+        extends BaseDBTest {
+
+    private static final String TAG = "AuthorParseTest";
+    private static final String UTF_8 = "UTF-8";
+
+    private DnbSearchEngine searchEngine;
+    private DnbAuthorResolver resolver;
+
+    @Before
+    public void setup()
+            throws DaoWriteException, StorageException {
+        super.setup(AppLocale.SYSTEM_LANGUAGE);
+
+        searchEngine = (DnbSearchEngine) EngineId.Dnb.createSearchEngine(context);
+        searchEngine.setCaller(new TestProgressListener(TAG));
+
+        searchEngine.getEngineId().getConfig().setLogHttpGetRequests(context, true);
+
+        resolver = (DnbAuthorResolver) DnbAuthorResolver.create(context, searchEngine);
+    }
+
+    @Test
+    public void liveParse128409142()
+            throws SearchException, CredentialsException, IOException {
+
+        final Author author = new Author("", "");
+        author.setIdentifierValue(Identifier.SID_DNB, 128409142);
+
+        final boolean resolved = resolver.resolve(context, author);
+        assertTrue(resolved);
+
+        parse128409142(author);
+    }
+
+    @Test
+    public void parse128409142()
+            throws SearchException, CredentialsException, IOException {
+
+        final String locationHeader = "https://katalog.dnb.de/DE/resource.html?id=128409142&pr=0&sortA=bez&sortD=-dat&v=plist";
+        final int resId = com.hardbacknutter.nevertoomanybooks.test
+                .R.raw.dnb_author_flix_128409142;
+
+        final Document document = loadDocument(resId, UTF_8, locationHeader);
+
+        final Author author = resolver.parse(context, document);
+        parse128409142(author);
+    }
+
+    private void parse128409142(@Nullable Author author) {
+        assertNotNull(author);
+
+        Log.d(TAG, author.toString());
+
+        assertEquals("Flix", author.getFamilyName());
+        assertEquals("", author.getGivenNames());
+
+        assertEquals(1, author.getIdentifiers().size());
+        Optional<String> oIv;
+        oIv = author.getIdentifierValue(Identifier.SID_DNB);
+        assertTrue(oIv.isPresent());
+        assertEquals("128409142", oIv.get());
+
+        author = author.getRealAuthor();
+        assertNotNull(author);
+
+        assertEquals("Görmann", author.getFamilyName());
+        assertEquals("Felix", author.getGivenNames());
+        oIv = author.getIdentifierValue(Identifier.SID_DNB);
+        assertTrue(oIv.isPresent());
+        assertEquals("1216065012", oIv.get());
+    }
+
+    @Test
+    public void parse1300021055()
+            throws IOException, SearchException, CredentialsException {
+        final String locationHeader = "https://katalog.dnb.de/DE/resource.html?id=1300021055&pr=0&sortA=bez&sortD=-dat&v=plist";
+        final int resId = com.hardbacknutter.nevertoomanybooks.test
+                .R.raw.dnb_author_1300021055;
+
+        final Document document = loadDocument(resId, UTF_8, locationHeader);
+
+        Author author = resolver.parse(context, document);
+        assertNotNull(author);
+
+        Log.d(TAG, author.toString());
+
+        assertEquals("von Humboldt", author.getFamilyName());
+        assertEquals("Dorothee", author.getGivenNames());
+        final Optional<String> oIv = author.getIdentifierValue(Identifier.SID_DNB);
+        assertTrue(oIv.isPresent());
+        assertEquals("1300021055", oIv.get());
+        author = author.getRealAuthor();
+        assertNull(author);
+    }
+}
---
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/dnb/DnbAuthorResolver.java
@@ -0,0 +1,243 @@
+/*
+ * @Copyright 2018-2025 HardBackNutter
+ * @License GNU General Public License
+ *
+ * This file is part of NeverTooManyBooks.
+ *
+ * NeverTooManyBooks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeverTooManyBooks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hardbacknutter.nevertoomanybooks.searchengines.dnb;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
+
+import java.util.Map;
+import java.util.Optional;
+
+import com.hardbacknutter.nevertoomanybooks.ServiceLocator;
+import com.hardbacknutter.nevertoomanybooks.core.network.CredentialsException;
+import com.hardbacknutter.nevertoomanybooks.core.network.HttpConstants;
+import com.hardbacknutter.nevertoomanybooks.core.tasks.Cancellable;
+import com.hardbacknutter.nevertoomanybooks.entities.Author;
+import com.hardbacknutter.nevertoomanybooks.entities.Identifier;
+import com.hardbacknutter.nevertoomanybooks.searchengines.AuthorResolver;
+import com.hardbacknutter.nevertoomanybooks.searchengines.EngineId;
+import com.hardbacknutter.nevertoomanybooks.searchengines.SearchEngine;
+import com.hardbacknutter.nevertoomanybooks.searchengines.SearchException;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * FIXME: we're a bit [bleeped] here due to using the beta website.
+ * The author permalink takes us to the old site which we don't parse
+ * because we mistakenly believed that DNB would actually finish the work on the beta...
+ * And we cannot follow the author to the beta site, as that requires
+ * a referer to be set or we get a 403 Forbidden.
+ * BUT... there is a workaround: setting referer to "https://katalog.dnb.de"
+ * seems to work.
+ * This needs more testing...
+ */
+public final class DnbAuthorResolver
+        implements AuthorResolver {
+
+    @NonNull
+    private final DnbSearchEngine searchEngine;
+    @Nullable
+    private final String authorUri;
+
+    /**
+     * Private Constructor.
+     *
+     * @param context      Current context
+     * @param searchEngine the engine
+     */
+    private DnbAuthorResolver(@NonNull final Context context,
+                              @NonNull final DnbSearchEngine searchEngine) {
+        this.searchEngine = searchEngine;
+        // hardcode the beta website
+        authorUri = "https://katalog.dnb.de/DE/resource.html?id=%s";
+        //        searchEngine.getEngineId()
+        //                                .getIdentifier().get()
+        //                                .getAuthorUri(context).orElse(null);
+    }
+
+    /**
+     * Private Constructor.
+     *
+     * @param context Current context
+     * @param caller  a {@link Cancellable} which can forward requests
+     *                to the (internal) {@link DnbSearchEngine}
+     */
+    @VisibleForTesting
+    private DnbAuthorResolver(@NonNull final Context context,
+                              @Nullable final Cancellable caller) {
+        this(context, (DnbSearchEngine) EngineId.Dnb.createSearchEngine(context));
+        searchEngine.setCaller(caller);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param context      Current context
+     * @param searchEngine the engine which is requesting this resolver
+     *
+     * @return new instance
+     */
+    @NonNull
+    public static AuthorResolver create(@NonNull final Context context,
+                                        @NonNull final SearchEngine searchEngine) {
+        if (searchEngine instanceof DnbSearchEngine) {
+            return new DnbAuthorResolver(context,
+                                         (DnbSearchEngine) searchEngine);
+        } else {
+            return new DnbAuthorResolver(context, searchEngine);
+        }
+    }
+
+    @Override
+    public boolean resolve(@NonNull final Context context,
+                           @NonNull final Author author)
+            throws SearchException, CredentialsException {
+        // the user can delete it...
+        if (authorUri == null) {
+            return false;
+        }
+
+        // If we already have a real-author set, we're done.
+        if (author.getRealAuthor() != null) {
+            return false;
+        }
+
+        final Optional<String> oIv = author.getIdentifierValue(Identifier.SID_DNB);
+        // no id, give up
+        if (oIv.isEmpty()) {
+            return false;
+        }
+
+        final String url = String.format(authorUri, oIv.get());
+
+        final Document document = searchEngine.loadDocument(context, url,
+                                                            Map.of(HttpConstants.REFERER,
+                                                                   "https://katalog.dnb.de"));
+        if (!searchEngine.isCancelled()) {
+
+            final Author found = parse(context, document);
+            if (found != null) {
+                author.setName(found.getFamilyName(), found.getGivenNames());
+                author.setRealAuthor(found.getRealAuthor());
+                author.addIdentifiers(found.getIdentifiers());
+                ServiceLocator.getInstance().getIdentifierDao()
+                              .pruneList(author.getIdentifiers());
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @VisibleForTesting
+    @Nullable
+    Author parse(@NonNull final Context context,
+                 @NonNull final Document document)
+            throws SearchException, CredentialsException {
+
+        Author author = null;
+        // relative url
+        String realAuthorUrl = null;
+        boolean isPseudonym = false;
+
+        final Element tableElement = document.selectFirst("table.c-catalog-table__table");
+        // We could collapse the above select with the select for the tr's
+        // But there is a tbody in between, let's take the safe route for now
+        if (tableElement != null) {
+            final Elements trs = tableElement.select("tr.c-catalog-table__row");
+            for (final Element tr : trs) {
+                final Element label = tr.selectFirst("th.c-catalog-table__head > p");
+                if (label != null) {
+                    final Element td = tr.selectFirst("td.c-catalog-table__content > p");
+                    if (td != null) {
+                        final String s = label.text();
+                        // We should get the german labels, but it seems we might get the
+                        // english ones despite the "DE" in the url. So... check on both!
+                        switch (s) {
+                            case "Name": {
+                                author = Author.from(td.text());
+                                break;
+                            }
+                            case "Wirklicher Name":
+                            case "Real name": {
+                                final Element a = td.selectFirst("a");
+                                if (a != null) {
+                                    realAuthorUrl = a.attr("href");
+                                }
+                                break;
+                            }
+                            case "Zeit":
+                            case "Time": {
+                                break;
+                            }
+                            case "Ort":
+                            case "Place": {
+                                break;
+                            }
+                            case "Geschlecht":
+                            case "Gender": {
+                                // männlich
+                                // weiblich
+                                break;
+                            }
+                            case "Datensatztyp":
+                            case "Record type": {
+                                // can be one of:
+                                // Person
+                                // Pseudonym
+                                isPseudonym = "Pseudonym".equals(td.text());
+                                break;
+                            }
+                            case "Datensatz-ID":
+                            case "Record ID": {
+                                if (author != null) {
+                                    author.setIdentifierValue(Identifier.SID_DNB, td.text());
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (author != null && isPseudonym && realAuthorUrl != null) {
+            final String url = searchEngine.getHostUrl(context) + '/' + realAuthorUrl;
+            // referer must be set or we get a 403 Forbidden
+            final Document raDoc = searchEngine.loadDocument(context, url,
+                                                             Map.of(HttpConstants.REFERER,
+                                                                    document.location()));
+            if (!searchEngine.isCancelled()) {
+                final Author realAuthor = parse(context, raDoc);
+                if (realAuthor != null) {
+                    author.setRealAuthor(realAuthor);
+                }
+            }
+        }
+
+        return author;
+    }
+}
