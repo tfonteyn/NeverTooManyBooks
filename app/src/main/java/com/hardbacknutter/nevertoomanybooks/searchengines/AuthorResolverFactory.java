8a0749dc5bc145bbb9e567300ca0613f67b7825f
hardbacknutter <user@localhost>
2025-04-14 17:09:04 +0100

    SSLProtocolException docs

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/AuthorResolverFactory.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/AuthorResolverFactory.java
@@ -98,7 +98,7 @@ public final class AuthorResolverFactory {
                     return List.of(DnbAuthorResolver.create(context, searchEngine));
                 }
                 case Isfdb: {
-                    // URGENT: 2025-03-25. ongoing site issues SSLProtocolException
+                    // URGENT: 2025-04-14. ongoing site issues SSLProtocolException
                     // Read error: ssl=0x7a6b6d87b598: Failure in SSL library, usually a protocol error
                     // error:1e000065:Cipher functions:OPENSSL_internal:BAD_DECRYPT
                     // (external/boringssl/src/crypto/cipher_extra/e_chacha20poly1305.c:259
@@ -106,8 +106,12 @@ public final class AuthorResolverFactory {
                     // error:1000008b:SSL routines:OPENSSL_internal:DECRYPTION_FAILED_OR_BAD_RECORD_MAC
                     // (external/boringssl/src/ssl/tls_record.cc:294 0x7a69b00547fb:0x00000000)
                     // |docRequestUrl="https://www.isfdb.org/cgi-bin/ea.cgi?5"
+                    //
+                    // we tried a clean disconnect, create new request, connect,
+                    // but the problem persists despite req param "connection=close"
+                    // so the issue seems entirely on the server side.
 
-                    //return List.of(IsfdbAuthorResolver.create(context, searchEngine));
+//                    return List.of(IsfdbAuthorResolver.create(context, searchEngine));
                     return List.of();
                 }
                 case OpenLibrary: {
