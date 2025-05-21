018230fb55ffed83b81da7f4b1b32148c50c513c
hardbacknutter <user@localhost>
2025-05-21 15:37:19 +0100

    docs

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/bedetheque/BedethequeAuthorResolver.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/bedetheque/BedethequeAuthorResolver.java
@@ -156,14 +156,17 @@ public class BedethequeAuthorResolver
             final Document document = searchEngine.loadDocument(context, url, null);
             if (!searchEngine.isCancelled()) {
                 final Author found = parse(context, document);
-                // We always check for name equality on this site to avoid mistakes
-                // from the other sites which can also use this resolver.
+                // We handle the check for name equality and the subsequent
+                // overwriting of the names different here as compared
+                // to other sites/resolvers.
+                // We do this due to:
+                // - majority of french names with diacritics
+                // - resolver is used by 3 other sites which often
+                //   use the wrong/lack diacritics
+                // Note we do NOT overwrite the 'realAuthor' name.
+                // as typically we just found them here.
                 if (found != null && author.isSameName(found)) {
-                    // Overwrite the original name; this will correct any diacritics
-                    // We're not doing the same for the 'realAuthor' ...
-                    // typically this will not be needed anyhow
-                    // but if it is... it's too difficult to be sure we're doing it right.
-                    // (might be a wrong assumption... oh well)
+                    // Overwrite the original name to correct any diacritics
                     author.setName(found.getFamilyName(), found.getGivenNames());
                     // and merge as normal
                     author.merge(found, true);
