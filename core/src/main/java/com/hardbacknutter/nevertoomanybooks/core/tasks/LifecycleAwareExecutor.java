cee869fceb3826e3cb34ee1c7e21faafb2f02b06
hardbacknutter <user@localhost>
2025-06-01 11:18:51 +0100

    add for future usage

---
+++ core/src/main/java/com/hardbacknutter/nevertoomanybooks/core/tasks/LifecycleAwareExecutor.java
@@ -0,0 +1,57 @@
+/*
+ * @Copyright 2018-2025 HardBackNutter
+ * @License GNU General Public License
+ *
+ * This file is part of NeverTooManyBooks.
+ *
+ * NeverTooManyBooks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeverTooManyBooks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hardbacknutter.nevertoomanybooks.core.tasks;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class LifecycleAwareExecutor
+        implements DefaultLifecycleObserver {
+
+    private final ThreadPoolExecutor executor;
+
+    private LifecycleAwareExecutor(@NonNull final ThreadPoolExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NonNull
+    public static LifecycleAwareExecutor attach(@NonNull final LifecycleOwner owner,
+                                                @NonNull final ThreadPoolExecutor executor) {
+        final LifecycleAwareExecutor wrapper = new LifecycleAwareExecutor(executor);
+        owner.getLifecycle().addObserver(wrapper);
+        return wrapper;
+    }
+
+    @Override
+    public void onDestroy(@NonNull final LifecycleOwner owner) {
+        executor.shutdownNow();
+        owner.getLifecycle().removeObserver(this);
+    }
+
+    public void execute(@NonNull final Runnable task) {
+        executor.execute(task);
+    }
+}
+
+
