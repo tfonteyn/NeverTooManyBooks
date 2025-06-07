23dd0c63b329d4d48e7a0e151882eab4db48630d
hardbacknutter <user@localhost>
2025-06-07 14:40:30 +0100

    add launchBookPager method

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/activityresultcontracts/DisplayBookLauncher.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/activityresultcontracts/DisplayBookLauncher.java
@@ -29,8 +29,11 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import com.hardbacknutter.nevertoomanybooks.BooksOnBookshelf;
 import com.hardbacknutter.nevertoomanybooks.BooksOnBookshelfViewModel;
@@ -38,11 +41,17 @@ import com.hardbacknutter.nevertoomanybooks.ServiceLocator;
 import com.hardbacknutter.nevertoomanybooks.booklist.RebuildBooklist;
 import com.hardbacknutter.nevertoomanybooks.core.utils.ParcelUtils;
 import com.hardbacknutter.nevertoomanybooks.database.DBKey;
+import com.hardbacknutter.nevertoomanybooks.database.dao.TocEntryDao;
 import com.hardbacknutter.nevertoomanybooks.entities.AuthorWork;
 import com.hardbacknutter.nevertoomanybooks.entities.Book;
 import com.hardbacknutter.nevertoomanybooks.entities.Bookshelf;
 import com.hardbacknutter.nevertoomanybooks.entities.TocEntry;
 
+/**
+ * Small wrapper to call the {@link ShowBookPagerContract}
+ * or to open a NEW {@link BooksOnBookshelf} instance
+ * to display a book or list of books based on a list of {@link AuthorWork}s.
+ */
 public class DisplayBookLauncher {
 
     @NonNull
@@ -53,6 +62,61 @@ public class DisplayBookLauncher {
                                        optionalActivityResultCallback) {
         this.launcher = fragment.registerForActivityResult(new ShowBookPagerContract(),
                                                            optionalActivityResultCallback);
+    }
+
+    /**
+     * Launch the Book pager screen with the list of books derived
+     * from the actual books and the books from TOCEntries.
+     *
+     * @param works     books and TOCEntries
+     * @param position  current position
+     * @param bookshelf current Bookshelf displayed by the BoB
+     *
+     * @throws IllegalArgumentException (debug)
+     */
+    public void launchBookPager(@NonNull final List<AuthorWork> works,
+                                final int position,
+                                @NonNull final Bookshelf bookshelf) {
+        final AuthorWork currentWork = works.get(position);
+        // Collect all actual books, removing duplicates
+        final Set<Long> bookIds = new LinkedHashSet<>();
+        final TocEntryDao tocEntryDao = ServiceLocator.getInstance().getTocEntryDao();
+        for (final AuthorWork work : works) {
+            switch (work.getWorkType()) {
+                case TocEntry:
+                    bookIds.addAll(tocEntryDao.getBookIds(work.getId()));
+                    break;
+                case BookLight:
+                case Book:
+                    bookIds.add(work.getId());
+                    break;
+            }
+        }
+        final List<Long> bookIdList = new ArrayList<>(bookIds);
+
+        final long bookId;
+        switch (currentWork.getWorkType()) {
+            case TocEntry:
+                final List<Long> bookIds1 = tocEntryDao.getBookIds(currentWork.getId());
+                // Paranoia....
+                if (bookIds1.isEmpty()) {
+                    // Orphaned TocEntry, no book to display
+                    return;
+                }
+                bookId = bookIds1.get(0);
+                break;
+            case BookLight:
+            case Book:
+                bookId = currentWork.getId();
+                break;
+            default:
+                throw new IllegalArgumentException(currentWork.toString());
+        }
+        final long currentWorkId = currentWork.getId();
+        final int pos = bookIdList.indexOf(bookId);
+
+        launcher.launch(new ShowBookPagerContract.Input(currentWorkId, bookshelf,
+                                                        pos, bookIdList));
     }
 
     /**
