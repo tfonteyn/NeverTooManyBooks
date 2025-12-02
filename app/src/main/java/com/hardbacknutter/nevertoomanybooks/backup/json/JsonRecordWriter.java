7155ef1bb094d393bdd0456ace38d63c6716c494
hardbacknutter <user@localhost>
2025-12-02 19:59:51 +0000

    fix: Collection

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/backup/json/JsonRecordWriter.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/backup/json/JsonRecordWriter.java
@@ -34,6 +34,7 @@ import java.io.Writer;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -322,7 +323,7 @@ public class JsonRecordWriter
                 progressListener.publishProgress(1, context.getString(
                         R.string.lbl_books));
 
-                final List<Pair<String, String>> list =
+                final Collection<Pair<String, String>> list =
                         ServiceLocator.getInstance().getDeletedBooksDao().getAll(null);
                 if (!list.isEmpty()) {
                     jsonData.put(RecordType.DeletedBooks.getName(),
