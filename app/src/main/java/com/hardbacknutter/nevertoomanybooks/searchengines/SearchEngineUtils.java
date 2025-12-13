95bd7b04cf4c6c22fb7b545116696671c61730e9
hardbacknutter <user@localhost>
2025-12-13 22:19:28 +0100

    github #213 expand/simplify the regex's

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/SearchEngineUtils.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/SearchEngineUtils.java
@@ -28,6 +28,32 @@ import java.util.regex.Pattern;
 
 public final class SearchEngineUtils {
 
+    /** All non-rendering characters to REMOVE. */
+    @SuppressWarnings("UnnecessaryUnicodeEscape")
+    private static final Pattern P1_INVISIBLE_CONTROLS = Pattern.compile(
+            // Category 'Format' (includes Bidi, Zero-Width joiners/non-joiners, etc.)
+            // Category 'Private Use'
+            // Category 'Not Assigned' (Unassigned characters)
+            "\\p{Cf}|\\p{Co}|\\p{Cn}"
+    );
+
+    /** Non-standard whitespace to replace with a space. */
+    private static final Pattern P2_ODD_WHITESPACE = Pattern.compile(
+            "["
+            // Non-Breaking Spaces
+            + "\u00A0\u202F\u205F"
+            // Quad/Figure Spaces
+            + "\u2000-\u200A"
+            // Ideographic Space
+            + "\u3000"
+            // Zero-Width No-Break Space (acts like a space)
+            + "\uFEFF"
+            + "]+"
+    );
+
+    /** Replace repeated/special whitespace characters with a single space. */
+    private static final Pattern P3_WHITESPACE_REDUCTION = Pattern.compile("\\s+");
+
     /** Fields can contain div tags which we remove to make the text shorter. */
     private static final Pattern DIV_PATTERN = Pattern.compile("(\n*\\s*<div>\\s*|\\s*</div>)");
     /** Convert "&amp;" to '&'. */
@@ -35,41 +61,11 @@ public final class SearchEngineUtils {
     /** a CR is replaced with a space. */
     private static final Pattern CR_LITERAL = Pattern.compile("\n", Pattern.LITERAL);
 
-    /** All non-rendering characters to REMOVE. */
-    @SuppressWarnings("UnnecessaryUnicodeEscape")
-    private static final Pattern INVISIBLE_CHARS = Pattern.compile(
-            "["
-            // Zero-width characters
-            + "\u200B\u200C\u200D\uFEFF"
-            // Word joiner, invisible ops
-            + "\u2060-\u2064"
-            // Combining grapheme joiner
-            + "\u034F"
-            // BiDi controls (LRE, RLE, etc.)
-            + "\u202A-\u202E"
-            // LRI/RLI/FSI/PDI
-            + "\u2066-\u2069"
-            // Variation Selectors 1–16
-            + "\uFE00-\uFE0F"
-            // CJK Variation Selectors
-            + "\\x{E0100}-\\x{E01EF}"
-            // Tag characters (subtag system)
-            + "\\x{E0000}-\\x{E007F}"
-            + "]"
-    );
-
-    /** Non-standard whitespace to replace with a space. */
-    private static final Pattern ODD_WHITESPACE = Pattern.compile(
-            "[\u00A0\u2000-\u200A\u3000]");
-
-    /** Replace repeated/special whitespace characters with a single space. */
-    private static final Pattern WHITESPACE = Pattern.compile(
-            "[\\s\u00A0\u2000-\u200A\u3000]+");
 
     /**
      * Trim extraneous punctuation from the end of titles, authors and other types of names.
      *
-     * @see #cleanName(CharSequence)
+     * @see #cleanName(String)
      */
     private static final Pattern END_PUNCTUATION_PATTERN =
             Pattern.compile("[,.':;`~@#$%^&*(\\-=_+]*$");
@@ -114,15 +110,19 @@ public final class SearchEngineUtils {
      * @return cleansed string
      */
     @NonNull
-    public static String cleanText(@NonNull final CharSequence s) {
-        // remove all unwanted invisible chars
-        String text = INVISIBLE_CHARS.matcher(s).replaceAll("");
-        // and replace all special whitespace with a regular single space.
-        text = ODD_WHITESPACE.matcher(text).replaceAll(" ");
+    public static String cleanText(@NonNull final String s) {
+        String text = s;
+        // Replace any cr/lf with a single space
+        text = CR_LITERAL.matcher(text).replaceAll(" ");
 
-        if (text.contains("\n")) {
-            text = CR_LITERAL.matcher(text).replaceAll(" ");
-        }
+        // Remove all unwanted invisible chars
+        text = P1_INVISIBLE_CONTROLS.matcher(text).replaceAll("");
+        // Replace all special whitespace with a regular single space.
+        text = P2_ODD_WHITESPACE.matcher(text).replaceAll(" ");
+        // Reduce all remaining multiple whitespace
+        text = P3_WHITESPACE_REDUCTION.matcher(text).replaceAll(" ");
+
+        // Special rules:
 
         // replace any html encoded ampersands.
         if (text.contains("&")) {
@@ -140,7 +140,7 @@ public final class SearchEngineUtils {
     }
 
     /**
-     * Variant of {@link #cleanText(CharSequence)} which does additional cleanup
+     * Variant of {@link #cleanText(String)} which does additional cleanup
      * specific to author names and titles.
      *
      * @param s to clean
@@ -148,7 +148,7 @@ public final class SearchEngineUtils {
      * @return cleansed string
      */
     @NonNull
-    public static String cleanName(@NonNull final CharSequence s) {
+    public static String cleanName(@NonNull final String s) {
         final String text = cleanText(s);
         // remove any junk characters from the end of the string
         return END_PUNCTUATION_PATTERN.matcher(text).replaceAll("").strip();
@@ -157,17 +157,22 @@ public final class SearchEngineUtils {
     /**
      * Sanitize a parameter string for use in a url search.
      *
-     * @param search to clean
+     * @param s to clean
      *
      * @return cleansed string
      */
     @NonNull
-    public static String encodeSearchString(@Nullable final CharSequence search) {
-        if (search == null || search.length() == 0) {
+    public static String encodeSearchString(@Nullable final String s) {
+        if (s == null || s.isEmpty()) {
             return "";
         }
 
-        final String result = WHITESPACE.matcher(search).replaceAll(" ");
-        return KEEP_PATTERN.matcher(result).replaceAll("");
+        String text = s;
+        // Replace all special whitespace with a regular single space.
+        text = P2_ODD_WHITESPACE.matcher(text).replaceAll(" ");
+        // Reduce all remaining multiple whitespace
+        text = P3_WHITESPACE_REDUCTION.matcher(text).replaceAll(" ");
+        // And keep only alphanumeric characters and the space
+        return KEEP_PATTERN.matcher(text).replaceAll("");
     }
 }
