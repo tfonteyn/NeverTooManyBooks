384e440ec6ba13c41dd0d7ee12f00773fb56a9d9
hardbacknutter <user@localhost>
2025-12-13 22:43:06 +0100

    cleanText simple test

---
+++ app/src/androidTest/java/com/hardbacknutter/nevertoomanybooks/searchengines/SearchEngineUtilsTest.java
@@ -0,0 +1,53 @@
+/*
+ * @Copyright 2018-2025 HardBackNutter
+ * @License GNU General Public License
+ *
+ * This file is part of NeverTooManyBooks.
+ *
+ * NeverTooManyBooks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeverTooManyBooks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NeverTooManyBooks. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hardbacknutter.nevertoomanybooks.searchengines;
+
+import com.hardbacknutter.nevertoomanybooks.BaseDBTest;
+import com.hardbacknutter.nevertoomanybooks.core.database.DaoWriteException;
+import com.hardbacknutter.nevertoomanybooks.core.storage.StorageException;
+import com.hardbacknutter.nevertoomanybooks.utils.AppLocale;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+@SuppressWarnings({"MissingJavadoc", "LongLine"})
+public class SearchEngineUtilsTest
+        extends BaseDBTest {
+
+    @Before
+    public void setup()
+            throws DaoWriteException, StorageException {
+        super.setup(AppLocale.SYSTEM_LANGUAGE);
+    }
+
+    @Test
+    public void process() {
+        String s, c;
+
+        // \u202E is the Right-to-Left Override (RLO) BiDi control
+        s = "Hello" + "\u202E" + " World" + "\u200E";
+        c = SearchEngineUtils.cleanText(s);
+        Assert.assertEquals("Hello World", c);
+    }
+}
+
+
