486638e585962b46b5a5964b26aae6f08b956f2f
hardbacknutter <user@localhost>
2026-01-16 19:09:06 +0000

    docs

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/bnf/BnfSearchEngine.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/bnf/BnfSearchEngine.java
@@ -62,6 +62,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+/**
+ *
+ * https://www.ifla.org/unimarc-updates/unimarc-bibliographic-format-manual-online-ed/
+ * https://www.transition-bibliographique.fr/unimarc/manuel-unimarc-format-bibliographique/
+ *
+ */
 public class BnfSearchEngine
         extends JsoupSearchEngineBase
         implements SearchEngine.ByIsbn,
@@ -90,19 +96,33 @@ public class BnfSearchEngine
 
     private static final Map<String, Integer> AUTHOR_CODES = Map.ofEntries(
             Map.entry("000", AuthorRole.UNKNOWN),
+            // Artist
             Map.entry("040", AuthorRole.ARTIST),
+            // Author
             Map.entry("070", AuthorRole.WRITER),
-            Map.entry("690", AuthorRole.WRITER),
+            // Author of afterword, postface, colophon, etc.
             Map.entry("075", AuthorRole.AFTERWORD),
+            // Author of introduction, etc.
             Map.entry("080", AuthorRole.INTRODUCTION),
+            // Collaborator
             Map.entry("205", AuthorRole.CONTRIBUTOR),
-            Map.entry("570", AuthorRole.CONTRIBUTOR),
+            // Editor
             Map.entry("340", AuthorRole.EDITOR),
+            // Graphic technician
             Map.entry("410", AuthorRole.COLORIST),
-            // overlap with "040"
+            // Illustrator; overlap with "040"
             Map.entry("440", AuthorRole.ARTIST),
+            // Narrator
             Map.entry("550", AuthorRole.NARRATOR),
+            // Other
+            Map.entry("570", AuthorRole.CONTRIBUTOR),
+            // Publishing director (Series editor)
+            Map.entry("651", AuthorRole.EDITOR),
+            // Scenarist (Author of a screenplay.)
+            Map.entry("690", AuthorRole.WRITER),
+            // Translator
             Map.entry("730", AuthorRole.TRANSLATOR),
+            // Typographer
             Map.entry("750", AuthorRole.LETTERING));
 
     private final AuthorResolverHelper authorResolverHelper;
@@ -339,8 +359,6 @@ public class BnfSearchEngine
                         break;
                     }
                     case "200": {
-                        // 200 1. $a Le Dieu du carnage $b Texte imprimé $f Yasmina Reza
-                        // $g présentation, notes, questions et après-texte établis...
                         final Map<Character, String> fields = parseUnimarcField(text);
                         final String s = fields.get('a');
                         if (s != null && !s.isEmpty()) {
@@ -389,6 +407,68 @@ public class BnfSearchEngine
                         break;
                     }
                     default:
+                        // skipping these fields which we've encountered
+                        // 000
+                        // 001 RECORD IDENTIFIER
+                        // 020 NATIONAL BIBLIOGRAPHY NUMBER
+                        // 039 (bnf) Numéro de notice récupérée d'un ancien système BnF
+                        // 073 INTERNATIONAL ARTICLE NUMBER (EAN)
+                        // 100 GENERAL PROCESSING DATA
+                        // 102 COUNTRY OF PUBLICATION OR PRODUCTION
+                        // 105 CODED DATA FIELD: TEXTUAL LANGUAGE MATERIAL, MONOGRAPHIC
+                        // 106 CODED DATA FIELD: TEXTUAL RESOURCE – FORM
+                        // 181 CODED DATA FIELD: CONTENT FORM
+                        // 182 CODED DATA FIELD: MEDIA TYPE
+                        //
+                        // 300 GENERAL NOTES
+                        //     unstructured
+                        // 304 NOTES PERTAINING TO TITLE AND STATEMENT OF RESPONSIBILITY
+                        //
+                        // 312 NOTES PERTAINING TO RELATED TITLES
+                        // 312 .. $a Autre forme de titre : De la colonisation aux indépendances
+                        //
+                        // 316 NOTE RELATING TO THE ITEM
+                        //
+                        // 321 EXTERNAL INDEXES/ABSTRACTS/REFERENCES NOTE
+                        // 321 .. $a Escales en littérature de jeunesse $c 2013
+                        //
+                        // 329 (bnf) is used to store critical reviews or analytical commentaries
+                        //     provided by La Joie par les livres
+                        //     (the BnF’s National Center for Children's Literature).
+                        //
+                        // 333 USERS/INTENDED AUDIENCE NOTE
+                        // 333 .. $a À partir de 3 ans $2 CNLJ $k Avis critique donné par le Centre national de la littérature pour la jeunesse
+                        //
+                        // 410 (links) SERIES
+                        // 423 (links) ISSUED WITH
+                        // 461 (links) SET
+                        //
+                        // 510 PARALLEL TITLE PROPER
+                        //
+                        // 600 PERSONAL NAME USED AS SUBJECT
+                        // 600 .| $3 11908875 $a Jodorowsky $b Alexandro $f 1929-.... $2 rameau
+                        // -> on a book ABOUT Jodorowsky
+                        //
+                        // 606 TOPICAL NAME USED AS SUBJECT
+                        // 606 .. $3 11932417 $a Mariage $3 11940497 $x Rites et cérémonies $2 rameau
+                        //
+                        // 608 FORM, GENRE OR PHYSICAL CHARACTERISTICS ACCESS POINT
+                        // 608 .. $a Bandes dessinées $2 CNLJ $k Avis critique donné par le Centre national de la littérature pour la jeunesse
+                        // 608 .. $a Albums $2 CNLJ $k Avis critique donné par le Centre national de la littérature pour la jeunesse
+                        //
+                        // 676 DEWEY DECIMAL CLASSIFICATION
+                        //
+                        // 686 OTHER CLASS NUMBERS
+                        // 686 .. $a 804 $2 Cadre de classement de la Bibliographie nationale française
+
+
+                        // 801 ORIGINATING SOURCE
+                        // 801 .0 $a FR $b FR-751131015 $c 20100412 $g AFNOR $h FRBNF42177275000000X $2 intermrc
+                        //
+                        // 856 ELECTRONIC LOCATION AND ACCESS
+                        // 856 .2 $u 164608 $b Première de couverture
+                        //
+                        // 900+ not defined in the unimarc manual
                         break;
                 }
             }
