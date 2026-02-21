ca532d6e734e04f0ef6551b724dd79e61933956e
hardbacknutter <user@localhost>
2026-02-21 12:56:34 +0000

    use TextNormalizer

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/bedetheque/BedethequeAuthorResolver.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/bedetheque/BedethequeAuthorResolver.java
@@ -32,10 +32,11 @@ import java.util.Optional;
 
 import com.hardbacknutter.nevertoomanybooks.ServiceLocator;
 import com.hardbacknutter.nevertoomanybooks.core.database.DaoWriteException;
-import com.hardbacknutter.nevertoomanybooks.core.database.SqlEncode;
 import com.hardbacknutter.nevertoomanybooks.core.network.CredentialsException;
 import com.hardbacknutter.nevertoomanybooks.core.storage.StorageException;
 import com.hardbacknutter.nevertoomanybooks.core.tasks.Cancellable;
+import com.hardbacknutter.nevertoomanybooks.core.utils.textnormaliser.TextNormalizer;
+import com.hardbacknutter.nevertoomanybooks.core.utils.textnormaliser.TextNormalizerFactory;
 import com.hardbacknutter.nevertoomanybooks.database.dao.BedethequeCacheDao;
 import com.hardbacknutter.nevertoomanybooks.entities.Author;
 import com.hardbacknutter.nevertoomanybooks.entities.Identifier;
@@ -83,6 +84,7 @@ public class BedethequeAuthorResolver
     @NonNull
     private final Locale locale;
     private final BedethequeCacheDao cacheDao;
+    private TextNormalizer textNormalizer;
 
     /**
      * Private Constructor.
@@ -96,6 +98,7 @@ public class BedethequeAuthorResolver
         locale = searchEngine.getLocale(context);
 
         cacheDao = ServiceLocator.getInstance().getBedethequeCacheDao();
+        textNormalizer = TextNormalizerFactory.create();
     }
 
     /**
@@ -249,7 +252,7 @@ public class BedethequeAuthorResolver
      * @return [0A-Z] of the first character
      */
     private char firstChar(@NonNull final CharSequence name) {
-        final String normalized = SqlEncode.normalize(String.valueOf(name.charAt(0)));
+        final String normalized = textNormalizer.normalize(String.valueOf(name.charAt(0)));
         if (normalized.isEmpty()) {
             return '0';
         }
