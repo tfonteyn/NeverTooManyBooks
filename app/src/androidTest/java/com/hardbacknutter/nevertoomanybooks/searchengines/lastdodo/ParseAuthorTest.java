9c63b19cb4fb1232e812a51d9c7272914c43e237
hardbacknutter <user@localhost>
2026-02-22 11:58:10 +0000

    move parser tests to androidTest to ensure they use ICU

--- app/src/androidTest/java/com/hardbacknutter/nevertoomanybooks/searchengines/lastdodo/ParseAuthorTest.java
+++ app/src/androidTest/java/com/hardbacknutter/nevertoomanybooks/searchengines/lastdodo/ParseAuthorTest.java
@@ -1,5 +1,5 @@
 /*
- * @Copyright 2018-2023 HardBackNutter
+ * @Copyright 2018-2026 HardBackNutter
  * @License GNU General Public License
  *
  * This file is part of NeverTooManyBooks.
@@ -30,6 +30,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SuppressWarnings("CheckStyle")
 class ParseAuthorTest {
 
     @NonNull
