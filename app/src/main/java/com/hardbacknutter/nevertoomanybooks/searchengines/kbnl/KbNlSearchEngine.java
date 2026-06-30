108bf0e701222f6c14d95a60c45f15ad8c86de05
hardbacknutter <user@localhost>
2026-06-30 09:31:57 +0100

    use getProductCode()

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/kbnl/KbNlSearchEngine.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/searchengines/kbnl/KbNlSearchEngine.java
@@ -261,8 +261,8 @@ public class KbNlSearchEngine
         }
 
         if (fetchCovers[0]) {
-            final ProductCode productCode = ISBN.parseISBN(book.getRawProductCode());
-            if (productCode.isIsbn()) {
+            final ProductCode productCode = book.getProductCode();
+            if (productCode != null && productCode.isIsbn()) {
                 final AltEdition edition = new AltEditionProductCode(productCode);
                 searchBestCoverByEdition(context, edition, 0).ifPresent(
                         fileSpec -> CoverFileSpecArray.setFileSpec(book, 0, fileSpec));
