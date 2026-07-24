d12cfdbb30b8b003f973c619c9d329c2b1d68de9
hardbacknutter <user@localhost>
2026-07-24 10:30:19 +0100

    add textAppearance attribute

--- app/src/main/java/com/hardbacknutter/nevertoomanybooks/widgets/DismissibleBanner.java
+++ app/src/main/java/com/hardbacknutter/nevertoomanybooks/widgets/DismissibleBanner.java
@@ -27,9 +27,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.AttrRes;
 import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StyleRes;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.card.MaterialCardView;
@@ -40,69 +42,126 @@ public class DismissibleBanner
         extends MaterialCardView {
 
     private TextView textView;
-    private MaterialButton btnClose;
     @Nullable
     private OnDismissListener dismissListener;
 
     public DismissibleBanner(@NonNull final Context context) {
-        super(context);
-        init(context, null);
+        this(context, null);
     }
 
     public DismissibleBanner(@NonNull final Context context,
                              @Nullable final AttributeSet attrs) {
-        super(context, attrs, com.google.android.material.R.attr.materialCardViewStyle);
-        init(context, attrs);
+        this(context, attrs, com.google.android.material.R.attr.materialCardViewStyle);
     }
 
     public DismissibleBanner(@NonNull final Context context,
                              @Nullable final AttributeSet attrs,
-                             final int defStyleAttr) {
+                             @AttrRes final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context, attrs);
+        init(context, attrs, defStyleAttr);
     }
 
     private void init(@NonNull final Context context,
-                      @Nullable final AttributeSet attrs) {
+                      @Nullable final AttributeSet attrs,
+                      @AttrRes final int defStyleAttr) {
         // Inflate the layout and attach it to the parent!
         LayoutInflater.from(context).inflate(R.layout.view_dismissible_banner, this, true);
 
         textView = findViewById(R.id.bannerText);
-        btnClose = findViewById(R.id.btnCloseBanner);
-
-        btnClose.setOnClickListener(v -> {
-            setVisibility(View.GONE);
-            if (dismissListener != null) {
-                dismissListener.onDismissed(this);
-            }
-        });
-
-        // Parse custom XML attributes if they exist
         if (attrs != null) {
-            final TypedArray typedArray = context
-                    .obtainStyledAttributes(attrs, R.styleable.DismissibleBanner);
+            final TypedArray typedArray = context.obtainStyledAttributes(
+                    attrs, R.styleable.DismissibleBanner, defStyleAttr, 0);
 
-            final String text = typedArray.getString(R.styleable.DismissibleBanner_android_text);
-            if (text != null) {
-                textView.setText(text);
+            try {
+                if (typedArray.hasValue(R.styleable.DismissibleBanner_android_text)) {
+                    textView.setText(typedArray.getString(
+                            R.styleable.DismissibleBanner_android_text));
+                }
+                final int textAppearanceResId = typedArray.getResourceId(
+                        R.styleable.DismissibleBanner_android_textAppearance, 0);
+                if (textAppearanceResId != 0) {
+                    textView.setTextAppearance(textAppearanceResId);
+                }
+            } finally {
+                typedArray.recycle();
             }
-            typedArray.recycle();
+        }
+
+        final MaterialButton btnClose = findViewById(R.id.btnCloseBanner);
+        btnClose.setOnClickListener(this::dismiss);
+    }
+
+    /**
+     * Dismiss the banner.
+     */
+    public void dismiss() {
+        dismiss(null);
+    }
+
+    private void dismiss(@Nullable final View v) {
+        setVisibility(View.GONE);
+        if (dismissListener != null) {
+            dismissListener.onDismissed(this, (MaterialButton) v);
         }
     }
 
+    /**
+     * Set the text on the TextView.
+     *
+     * @param text text to be displayed
+     *
+     * @see TextView#setText(int)
+     */
     public void setText(@Nullable final CharSequence text) {
         textView.setText(text);
     }
 
+    /**
+     * Set the colour for the text in the TextView.
+     *
+     * @param color A color value in the form 0xAARRGGBB.
+     *              Do not pass a resource ID. To get a color value from a resource ID, call
+     *              {@link androidx.core.content.ContextCompat#getColor(Context, int) getColor}.
+     *
+     * @see TextView#setTextColor(int)
+     */
     public void setTextColor(@ColorInt final int color) {
         textView.setTextColor(color);
     }
 
+    /**
+     * Set the text style for the TextView.
+     *
+     * @param resId the resource identifier of the style to apply
+     *
+     * @see TextView#setTextAppearance(int)
+     */
+    public void setTextAppearance(@StyleRes final int resId) {
+        textView.setTextAppearance(resId);
+    }
+
+    /**
+     * Set a listener to be notified when the user taps the close-button.
+     *
+     * @param listener to use
+     */
     public void setOnDismissListener(@Nullable final OnDismissListener listener) {
         this.dismissListener = listener;
     }
 
+    /**
+     * Listener interface for {@link #setOnDismissListener(OnDismissListener)}.
+     */
+    @FunctionalInterface
     public interface OnDismissListener {
-        void onDismissed(@NonNull DismissibleBanner v);
+
+        /**
+         * Called when the banner is dismissed.
+         *
+         * @param banner The {@link DismissibleBanner}
+         * @param button The button tapped, or {@code null} if {@link #dismiss()} was called.
+         */
+        void onDismissed(@NonNull DismissibleBanner banner,
+                         @Nullable MaterialButton button);
     }
 }
